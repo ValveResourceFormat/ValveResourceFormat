ac7c401a94205f83a5ebd608011a8d97315aa841
Pavel Djundik <github@xpaw.me>
2015-09-16 13:31:30 +0300

    Read REDI.ExtraFloatData and REDI.ExtraStringData

--- ValveResourceFormat/Blocks/ResourceEditInfoStructs/ExtraFloatData.cs
+++ ValveResourceFormat/Blocks/ResourceEditInfoStructs/ExtraFloatData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -6,11 +7,36 @@ namespace ValveResourceFormat.Blocks.ResourceEditInfoStructs
 {
     public class ExtraFloatData : REDIBlock
     {
+        public class EditFloatData
+        {
+            public string Name { get; set; } 
+            public float Float { get; set; }
+        }
+
+        public List<EditFloatData> List;
+
+        public ExtraFloatData()
+        {
+            List = new List<EditFloatData>();
+        }
+
         public override void Read(BinaryReader reader)
         {
             reader.BaseStream.Position = this.Offset;
 
+            for (var i = 0; i < this.Size; i++)
+            {
+                var dep = new EditFloatData();
 
+                var prev = reader.BaseStream.Position;
+                reader.BaseStream.Position += reader.ReadUInt32();
+                dep.Name = reader.ReadNullTermString(Encoding.UTF8);
+                reader.BaseStream.Position = prev + 4;
+
+                dep.Float = reader.ReadSingle();
+
+                List.Add(dep);
+            }
         }
 
         public override string ToString()
@@ -22,10 +48,17 @@ namespace ValveResourceFormat.Blocks.ResourceEditInfoStructs
         {
             var str = new StringBuilder();
 
-            str.AppendFormat("{0}Struct m_ExtraFloatData[{1}] = \n", indent, 0);
+            str.AppendFormat("{0}Struct m_ExtraFloatData[{1}] = \n", indent, List.Count);
             str.AppendFormat("{0}[\n", indent);
 
-            // TODO
+            foreach (var dep in List)
+            {
+                str.AppendFormat("{0}\tResourceEditFloatData_t\n", indent);
+                str.AppendFormat("{0}\t{{\n", indent);
+                str.AppendFormat("{0}\t\tCResourceString m_Name = \"{1}\"\n", indent, dep.Name);
+                str.AppendFormat("{0}\t\tfloat32 m_flFloat = {1:F6}\n", indent, dep.Float);
+                str.AppendFormat("{0}\t}}\n", indent);
+            }
 
             str.AppendFormat("{0}]\n", indent);
 
--- ValveResourceFormat/Blocks/ResourceEditInfoStructs/ExtraStringData.cs
+++ ValveResourceFormat/Blocks/ResourceEditInfoStructs/ExtraStringData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -6,11 +7,39 @@ namespace ValveResourceFormat.Blocks.ResourceEditInfoStructs
 {
     public class ExtraStringData : REDIBlock
     {
+        public class EditStringData
+        {
+            public string Name { get; set; } 
+            public string String { get; set; }
+        }
+
+        public List<EditStringData> List;
+
+        public ExtraStringData()
+        {
+            List = new List<EditStringData>();
+        }
+
         public override void Read(BinaryReader reader)
         {
             reader.BaseStream.Position = this.Offset;
 
+            for (var i = 0; i < this.Size; i++)
+            {
+                var dep = new EditStringData();
 
+                var prev = reader.BaseStream.Position;
+                reader.BaseStream.Position += reader.ReadUInt32();
+                dep.Name = reader.ReadNullTermString(Encoding.UTF8);
+                reader.BaseStream.Position = prev + 4;
+
+                prev = reader.BaseStream.Position;
+                reader.BaseStream.Position += reader.ReadUInt32();
+                dep.String = reader.ReadNullTermString(Encoding.UTF8);
+                reader.BaseStream.Position = prev + 4;
+
+                List.Add(dep);
+            }
         }
 
         public override string ToString()
@@ -22,10 +51,17 @@ namespace ValveResourceFormat.Blocks.ResourceEditInfoStructs
         {
             var str = new StringBuilder();
 
-            str.AppendFormat("{0}Struct m_ExtraStringData[{1}] = \n", indent, 0);
+            str.AppendFormat("{0}Struct m_ExtraStringData[{1}] = \n", indent, List.Count);
             str.AppendFormat("{0}[\n", indent);
 
-            // TODO
+            foreach (var dep in List)
+            {
+                str.AppendFormat("{0}\tResourceEditStringData_t\n", indent);
+                str.AppendFormat("{0}\t{{\n", indent);
+                str.AppendFormat("{0}\t\tCResourceString m_Name = \"{1}\"\n", indent, dep.Name);
+                str.AppendFormat("{0}\t\tCResourceString m_String = \"{1}\"\n", indent, dep.String);
+                str.AppendFormat("{0}\t}}\n", indent);
+            }
 
             str.AppendFormat("{0}]\n", indent);
 
