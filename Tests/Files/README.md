3ff18ec6eb7734f07d4e2bfa1122f3538cc04dc8
Pavel Djundik <github@xpaw.me>
2015-09-24 11:27:34 +0300

    Create testing project (it only tries to read all files at the moment)

---
+++ Tests/Files/README.md
@@ -0,0 +1,1 @@
+These files were extracted from Dota 2 VPKs for testing purposes only.
---
+++ Tests/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using ValveResourceFormat;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        private List<Resource> Resources = new List<Resource>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files");
+            var files = Directory.GetFiles(path, "*.*_c");
+
+            if (files.Length == 0)
+            {
+                Assert.Fail("There are no files to test.");
+            }
+
+            foreach (var file in files)
+            {
+                Console.WriteLine("Reading \"{0}\"...", file);
+
+                var resource = new Resource();
+                resource.Read(file);
+
+                Resources.Add(resource);
+
+                Console.WriteLine("\tOK");
+            }
+        }
+
+        [Test]
+        public void TestCase()
+        {
+            
+        }
+    }
+}
