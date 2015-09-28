a7fed2497a92c0518ba00969ddabfe7925bbbd56
Pavel Djundik <github@xpaw.me>
2015-09-28 16:24:44 +0300

    Test decompiler output against Valve's resourceinfo

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using ValveResourceFormat;
+using System.Text.RegularExpressions;
 
 namespace Tests
 {
     [TestFixture]
     public class Test
     {
-        private List<Resource> Resources = new List<Resource>();
+        private Dictionary<string, Resource> Resources;
+
+        public Test()
+        {
+            Resources = new Dictionary<string, Resource>();
+        }
 
         [SetUp]
         public void SetUp()
@@ -31,16 +37,53 @@ namespace Tests
                 var resource = new Resource();
                 resource.Read(file);
 
-                Resources.Add(resource);
+                Resources.Add(Path.GetFileName(file), resource);
 
                 Console.WriteLine("\tOK");
             }
         }
 
         [Test]
-        public void TestCase()
+        public void VerifyBlocks()
         {
-            
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "ValidOutput");
+            var files = Directory.GetFiles(path, "*.*txt", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var name = Path.GetFileName(Path.GetDirectoryName(file));
+
+                if (!Resources.ContainsKey(name))
+                {
+                    Console.WriteLine("{0}: no such resource", name);
+
+                    continue;
+                }
+
+                var resource = Resources[name];
+                var blockName = Path.GetFileNameWithoutExtension(file);
+
+                BlockType blockType;
+                Enum.TryParse(blockName, false, out blockType);
+
+                if (!resource.Blocks.ContainsKey(blockType))
+                {
+                    Console.WriteLine("{0}: no such block: {1}", name, blockType);
+
+                    continue;
+                }
+
+                Console.WriteLine("{0}: Testing {1} block...", name, blockType);
+
+                var actualOutput = resource.Blocks[blockType].ToString();
+                var expectedOutput = File.ReadAllText(file);
+
+                // We don't care about Valve's messy whitespace, so just strip it.
+                actualOutput = Regex.Replace(actualOutput, @"\s+", String.Empty);
+                expectedOutput = Regex.Replace(expectedOutput, @"\s+", String.Empty);
+
+                Assert.AreEqual(actualOutput, expectedOutput);
+            }
         }
     }
 }
