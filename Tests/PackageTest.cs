d9092b11fbd557631c7a04f8e0d8c0b8484e0188
Pavel Djundik <github@xpaw.me>
2015-09-28 15:33:10 +0300

    Add vpk reading test

---
+++ Tests/PackageTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ValveResourceFormat;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PackageTest
+    {
+        [Test]
+        public void ParseVPK()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "platform_misc_dir.vpk");
+
+            var package = new Package();
+            package.Read(path);
+        }
+    }
+}
