a543f8713946eed0cd44e41416159fbc07b4845d
Trent Monahan <sinz@d3s.co>
2015-10-10 21:34:59 +1100

    Initial UI commit

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainTabs = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(141, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // mainTabs
+            // 
+            this.mainTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabs.Location = new System.Drawing.Point(0, 33);
+            this.mainTabs.Name = "mainTabs";
+            this.mainTabs.SelectedIndex = 0;
+            this.mainTabs.Size = new System.Drawing.Size(778, 511);
+            this.mainTabs.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.mainTabs);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Valve Resource Format";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TabControl mainTabs;
+    }
+}
+
---
+++ GUI/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ValveResourceFormat;
+using ValveResourceFormat.ResourceTypes;
+
+namespace GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Valve Resource Format (*.*_c)|*.*_c|Valve Package Format (*.vpk)|*.vpk";
+            openDialog.FilterIndex = 1;
+            var userOK = openDialog.ShowDialog();
+            if (userOK == DialogResult.OK)
+            {
+                mainTabs.TabPages.Clear();
+                if (openDialog.FileName.EndsWith(".vpk")) {
+                    var package = new Package();
+
+                    try
+                    {
+                        package.Read(openDialog.FileName);
+                        var tab = new TabPage("Files");
+                        var control = new TreeView();
+                        control.Dock = DockStyle.Fill;
+
+                        //http://stackoverflow.com/a/24591871
+                        TreeNode currentnode;
+                        foreach (var filetype in package.Entries)
+                        {
+                            foreach (var file in filetype.Value)
+                            {
+                                currentnode = null;
+                                foreach (string subPath in (file.DirectoryName + Path.DirectorySeparatorChar + file.FileName + "." + filetype.Key).Split(Path.DirectorySeparatorChar))
+                                {
+                                    if (currentnode == null) //Root directory
+                                    {
+                                        if (subPath == " ")
+                                            continue; //root files
+                                        if (null == control.Nodes[subPath])
+                                            currentnode = control.Nodes.Add(subPath, subPath);
+                                        else
+                                            currentnode = control.Nodes[subPath];
+                                    }
+                                    else //Not root directory
+                                    {
+                                        if (null == currentnode.Nodes[subPath])
+                                            currentnode = currentnode.Nodes.Add(subPath, subPath);
+                                        else
+                                            currentnode = currentnode.Nodes[subPath];
+                                    }
+                                }
+                            }
+                        }
+                        control.Sort();
+                        control.ExpandAll();
+                        tab.Controls.Add(control);
+                        mainTabs.TabPages.Add(tab);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                } else {
+                    var resource = new Resource();
+                    resource.Read(openDialog.FileName);
+                    this.Text = "Valve Resource Format - " + openDialog.FileName;
+
+                    switch (resource.ResourceType)
+                    {
+                        case ResourceType.Texture:
+                            var tab = new TabPage("TEXTURE");
+                            var control = new PictureBox();
+                            control.Image = ((Texture)resource.Blocks[BlockType.DATA]).GenerateBitmap();
+                            control.Dock = DockStyle.Fill;
+                            tab.Controls.Add(control);
+                            mainTabs.TabPages.Add(tab);
+                            break;
+                    }
+                    foreach (var block in resource.Blocks)
+                    {
+                        var tab = new TabPage(block.Key.ToString());
+                        var control = new TextBox();
+                        control.Text = block.Value.ToString().Replace("\n", Environment.NewLine); //make sure panorama is new lines
+                        control.Dock = DockStyle.Fill;
+                        control.Multiline = true;
+                        control.ReadOnly = true;
+                        control.ScrollBars = ScrollBars.Both;
+                        tab.Controls.Add(control);
+                        mainTabs.TabPages.Add(tab);
+                    }
+                }
+            }
+        }
+    }
+}
