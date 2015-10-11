3b784b439604aadd1b137d4a97dde6f1bfa84148
Pavel Djundik <github@xpaw.me>
2015-10-11 20:53:58 +0300

    Load up resource in a task

--- GUI/MainForm.cs
+++ GUI/MainForm.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ValveResourceFormat;
@@ -42,141 +41,160 @@ namespace GUI
 
             if (userOK == DialogResult.OK)
             {
-                mainTabs.TabPages.Clear();
+                var task = Task.Factory.StartNew(() => ProcessFile(openDialog));
 
-                if (openDialog.FileName.EndsWith(".vpk", StringComparison.Ordinal))
+                task.ContinueWith(t =>
                 {
-                    var package = new Package();
-
-                    try
+                    t.Exception.Flatten().Handle(ex =>
                     {
-                        package.Read(openDialog.FileName);
+                        MessageBox.Show(ex.Message, "Failed to read package", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-                        var tab = new TabPage("Files");
-                        var control = new TreeView();
-                        control.Dock = DockStyle.Fill;
-                        control.ImageList = ImageList;
+                        return true;
+                    });
+                }, TaskContinuationOptions.OnlyOnFaulted);
+                
+                task.ContinueWith(t =>
+                {
+                    mainTabs.TabPages.Clear();
 
-                        //http://stackoverflow.com/a/24591871
-                        TreeNode currentnode;
+                    foreach (var tab in t.Result)
+                    {
+                        mainTabs.TabPages.Add(tab);
+                    }
+                }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
 
-                        foreach (var filetype in package.Entries)
+        private List<TabPage> ProcessFile(FileDialog openDialog)
+        {
+            var tabs = new List<TabPage>();
+
+            if (openDialog.FileName.EndsWith(".vpk", StringComparison.Ordinal))
+            {
+                var package = new Package();
+                package.Read(openDialog.FileName);
+
+                var tab = new TabPage("Files");
+                var control = new TreeView();
+                control.Dock = DockStyle.Fill;
+                control.ImageList = ImageList;
+
+                //http://stackoverflow.com/a/24591871
+                TreeNode currentnode;
+
+                foreach (var filetype in package.Entries)
+                {
+                    foreach (var file in filetype.Value)
+                    {
+                        currentnode = null;
+
+                        foreach (string subPath in (file.DirectoryName + Path.DirectorySeparatorChar + file.FileName + "." + filetype.Key).Split(Path.DirectorySeparatorChar))
                         {
-                            foreach (var file in filetype.Value)
+                            if (currentnode == null) //Root directory
                             {
-                                currentnode = null;
-
-                                foreach (string subPath in (file.DirectoryName + Path.DirectorySeparatorChar + file.FileName + "." + filetype.Key).Split(Path.DirectorySeparatorChar))
+                                if (subPath == " ")
                                 {
-                                    if (currentnode == null) //Root directory
-                                    {
-                                        if (subPath == " ")
-                                        {
-                                            continue; //root files
-                                        }
+                                    continue; //root files
+                                }
 
-                                        if (null == control.Nodes[subPath])
-                                        {
-                                            currentnode = control.Nodes.Add(subPath, subPath);
-                                        }
-                                        else
-                                        {
-                                            currentnode = control.Nodes[subPath];
-                                        }
-                                    }
-                                    else //Not root directory
-                                    {
-                                        if (null == currentnode.Nodes[subPath])
-                                        {
-                                            currentnode = currentnode.Nodes.Add(subPath, subPath);
-                                        }
-                                        else
-                                        {
-                                            currentnode = currentnode.Nodes[subPath];
-                                        }
-                                    }
+                                if (null == control.Nodes[subPath])
+                                {
+                                    currentnode = control.Nodes.Add(subPath, subPath);
+                                }
+                                else
+                                {
+                                    currentnode = control.Nodes[subPath];
+                                }
+                            }
+                            else //Not root directory
+                            {
+                                if (null == currentnode.Nodes[subPath])
+                                {
+                                    currentnode = currentnode.Nodes.Add(subPath, subPath);
+                                }
+                                else
+                                {
+                                    currentnode = currentnode.Nodes[subPath];
+                                }
+                            }
 
-                                    var ext = Path.GetExtension(currentnode.Name);
+                            var ext = Path.GetExtension(currentnode.Name);
 
-                                    if (ext.Length == 0)
-                                    {
-                                        ext = "_folder";
-                                    }
-                                    else
+                            if (ext.Length == 0)
+                            {
+                                ext = "_folder";
+                            }
+                            else
+                            {
+                                ext = ext.Substring(1);
+
+                                if (ext.EndsWith("_c", StringComparison.Ordinal))
+                                {
+                                    ext = ext.Substring(0, ext.Length - 2);
+                                }
+
+                                if (!ImageList.Images.ContainsKey(ext))
+                                {
+                                    if (ext[0] == 'v')
                                     {
                                         ext = ext.Substring(1);
 
-                                        if (ext.EndsWith("_c", StringComparison.Ordinal))
-                                        {
-                                            ext = ext.Substring(0, ext.Length - 2);
-                                        }
-
                                         if (!ImageList.Images.ContainsKey(ext))
                                         {
-                                            if(ext[0] == 'v')
-                                            {
-                                                ext = ext.Substring(1);
-
-                                                if (!ImageList.Images.ContainsKey(ext))
-                                                {
-                                                    ext = "_default";
-                                                }
-                                            }
-                                            else
-                                            {
-                                                ext = "_default";
-                                            }
+                                            ext = "_default";
                                         }
                                     }
-
-                                    currentnode.ImageKey = ext;
-                                    currentnode.SelectedImageKey = ext;
+                                    else
+                                    {
+                                        ext = "_default";
+                                    }
                                 }
                             }
-                        }
 
-                        control.Sort();
-                        control.ExpandAll();
-                        tab.Controls.Add(control);
-                        mainTabs.TabPages.Add(tab);
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message, "Failed to read package", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            currentnode.ImageKey = ext;
+                            currentnode.SelectedImageKey = ext;
+                        }
                     }
                 }
-                else
+
+                control.Sort();
+                control.ExpandAll();
+                tab.Controls.Add(control);
+                tabs.Add(tab);
+            }
+            else
+            {
+                var resource = new Resource();
+                resource.Read(openDialog.FileName);
+                this.Text = "Valve Resource Format - " + openDialog.FileName;
+
+                switch (resource.ResourceType)
                 {
-                    var resource = new Resource();
-                    resource.Read(openDialog.FileName);
-                    this.Text = "Valve Resource Format - " + openDialog.FileName;
-
-                    switch (resource.ResourceType)
-                    {
-                        case ResourceType.Texture:
-                            var tab = new TabPage("TEXTURE");
-                            var control = new PictureBox();
-                            control.Image = ((Texture)resource.Blocks[BlockType.DATA]).GenerateBitmap();
-                            control.Dock = DockStyle.Fill;
-                            tab.Controls.Add(control);
-                            mainTabs.TabPages.Add(tab);
-                            break;
-                    }
-
-                    foreach (var block in resource.Blocks)
-                    {
-                        var tab = new TabPage(block.Key.ToString());
-                        var control = new TextBox();
-                        control.Text = block.Value.ToString().Replace("\n", Environment.NewLine); //make sure panorama is new lines
+                    case ResourceType.Texture:
+                        var tab = new TabPage("TEXTURE");
+                        var control = new PictureBox();
+                        control.Image = ((Texture)resource.Blocks[BlockType.DATA]).GenerateBitmap();
                         control.Dock = DockStyle.Fill;
-                        control.Multiline = true;
-                        control.ReadOnly = true;
-                        control.ScrollBars = ScrollBars.Both;
                         tab.Controls.Add(control);
                         mainTabs.TabPages.Add(tab);
-                    }
+                        break;
+                }
+
+                foreach (var block in resource.Blocks)
+                {
+                    var tab = new TabPage(block.Key.ToString());
+                    var control = new TextBox();
+                    control.Text = block.Value.ToString().Replace("\n", Environment.NewLine); //make sure panorama is new lines
+                    control.Dock = DockStyle.Fill;
+                    control.Multiline = true;
+                    control.ReadOnly = true;
+                    control.ScrollBars = ScrollBars.Both;
+                    tab.Controls.Add(control);
+                    tabs.Add(tab);
                 }
             }
+
+            return tabs;
         }
     }
 }
