9aa5f18b07a763de027d8142af93db3f3c5abf99
Trent Monahan <sinz@d3s.co>
2015-10-29 22:42:17 +1100

    Added the programs icon to MainForm

--- GUI/MainForm.Designer.cs
+++ GUI/MainForm.Designer.cs
@@ -141,6 +141,7 @@
             this.ClientSize = new System.Drawing.Size(944, 461);
             this.Controls.Add(this.mainTabs);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(300, 300);
