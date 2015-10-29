4e747dbfa49497acc51f776d2f5c98fa3874971c
Pavel Djundik <github@xpaw.me>
2015-10-29 15:15:26 +0200

    Add tests for each texture type

---
+++ Tests/TextureTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using NUnit.Framework;
+using ValveResourceFormat;
+using ValveResourceFormat.ResourceTypes;
+
+namespace Tests
+{
+    public class TextureTests
+    {
+        [Test]
+        public void Test()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "Textures");
+            var files = Directory.GetFiles(path, "*.vtex_c");
+
+            foreach (var file in files)
+            {
+                var resource = new Resource();
+                resource.Read(file);
+
+                var bitmap = ((Texture)resource.Blocks[BlockType.DATA]).GenerateBitmap();
+
+                using (var ms = new MemoryStream())
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+
+                    using (var expected = new FileStream(Path.ChangeExtension(file, "png"), FileMode.Open, FileAccess.Read))
+                    {
+                        FileAssert.AreEqual(expected, ms);
+                    }
+                }
+            }
+        }
+    }
+}
