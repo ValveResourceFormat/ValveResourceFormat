1ca0a8966f241d428cfdfb0f11c6583f531ac40a
Martin Benjamins <marlamin@marlamin.com>
2016-03-13 17:25:49 +0100

    Some very broken normal mapping implemention

--- GUI/Types/Renderer/ShaderLoader.cs
+++ GUI/Types/Renderer/ShaderLoader.cs
@@ -69,8 +69,16 @@ uniform sampler2D normalTexture;
 
 void main()
 {
-    if(texture2D(colorTexture, vTexCoordOut).a <= alphaReference) discard;
-    outputColor = texture(colorTexture, vTexCoordOut);
+    vec3 normal = normalize(texture2D(normalTexture, vTexCoordOut).rgb * 2.0 - 1.0);  
+  
+    vec3 light_pos = normalize(vec3(1.0, 1.0, 1.5));  
+  
+    float diffuse = max(dot(normal, light_pos), 0.0);  
+  
+    vec3 color = diffuse * texture2D(colorTexture, vTexCoordOut).rgb;  
+
+    // if(texture2D(colorTexture, vTexCoordOut).a <= alphaReference) discard;
+    outputColor = vec4(color, 1.0);
 }
 ";
             GL.ShaderSource(fragmentShader, fragmentShaderSource);
