101ac438846a8eb10350cdd5b23a371393e26e5c
Martin Benjamins <marlamin@marlamin.com>
2016-03-26 23:00:39 +0100

    Move some things around

--- ValveResourceFormat/CompiledShader/CompiledShader.cs
+++ ValveResourceFormat/CompiledShader/CompiledShader.cs
@@ -236,59 +236,54 @@ namespace ValveResourceFormat
             // Should have reached the offset to number of LZMA chunks (90272 for hero_pc_40_ps)
             Console.WriteLine("Offset: {0}", Reader.BaseStream.Position);
 
-            //ReadShaderChunk();
-        }
-
-        private void ReadShaderChunk()
-        {
-            // Steam\steamapps\common\dota 2 beta\game\dota\shaders\vfx\hero_pc_40_ps.vcs
-            Reader.BaseStream.Position = 90272; // Offset to number of LZMA chunks
             var lzmaCount = Reader.ReadUInt32();
 
             Reader.BaseStream.Position = 237892; // Start of offsets
-            var lzmaOffsets = new uint[lzmaCount];
+            var lzmaOffsets = new int[lzmaCount];
 
             for (int i = 0; i < lzmaCount; i++)
             {
-                lzmaOffsets[i] = Reader.ReadUInt32();
+                lzmaOffsets[i] = Reader.ReadInt32();
             }
 
-            // Yes, well aware that I can do reading of offsets and parsing in one go, but this is super temp
             for (int i = 0; i < lzmaCount; i++)
             {
-                Reader.BaseStream.Position = lzmaOffsets[i];
+            }
+        }
 
-                var chunkSize = Reader.ReadUInt32();
+        private byte[] ReadShaderChunk(int offset)
+        {
+            var prevPos = Reader.BaseStream.Position;
 
-                if (Reader.ReadUInt32() != 0x414D5A4C)
-                {
-                    throw new InvalidDataException("Not LZMA?");
-                }
+            Reader.BaseStream.Position = offset;
 
-                var uncompressedSize = Reader.ReadUInt32();
-                var compressedSize = Reader.ReadUInt32();
+            var chunkSize = Reader.ReadUInt32();
 
-                Console.WriteLine("Chunk size: {0}", chunkSize);
-                Console.WriteLine("Compressed size: {0}", compressedSize);
-                Console.WriteLine("Uncompressed size: {0} ({1:P2} compression)", uncompressedSize, (uncompressedSize - compressedSize) / (double)uncompressedSize);
+            if (Reader.ReadUInt32() != 0x414D5A4C)
+            {
+                throw new InvalidDataException("Not LZMA?");
+            }
 
-                var decoder = new SevenZip.Compression.LZMA.Decoder();
-                decoder.SetDecoderProperties(Reader.ReadBytes(5));
+            var uncompressedSize = Reader.ReadUInt32();
+            var compressedSize = Reader.ReadUInt32();
 
-                var compressedBuffer = Reader.ReadBytes((int)compressedSize);
+            Console.WriteLine("Chunk size: {0}", chunkSize);
+            Console.WriteLine("Compressed size: {0}", compressedSize);
+            Console.WriteLine("Uncompressed size: {0} ({1:P2} compression)", uncompressedSize, (uncompressedSize - compressedSize) / (double)uncompressedSize);
 
-                using (var inputStream = new MemoryStream(compressedBuffer))
-                using (var outStream = new MemoryStream((int)uncompressedSize))
-                {
-                    decoder.Code(inputStream, outStream, compressedBuffer.Length, uncompressedSize, null);
+            var decoder = new SevenZip.Compression.LZMA.Decoder();
+            decoder.SetDecoderProperties(Reader.ReadBytes(5));
 
-                    var outData = outStream.ToArray();
-                    // Let's not dump all shaders to desktop shall we
-                    if (i == 1)
-                    {
-                        File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "shader_out_" + i + ".bin"), outData);
-                    }
-                }
+            var compressedBuffer = Reader.ReadBytes((int)compressedSize);
+
+            Reader.BaseStream.Position = prevPos;
+
+            using (var inputStream = new MemoryStream(compressedBuffer))
+            using (var outStream = new MemoryStream((int)uncompressedSize))
+            {
+                decoder.Code(inputStream, outStream, compressedBuffer.Length, uncompressedSize, null);
+
+                return outStream.ToArray();
             }
         }
     }
