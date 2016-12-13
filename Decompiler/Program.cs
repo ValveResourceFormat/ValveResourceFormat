ebe7ac64a5e3654624f17b033a3e3ecb338833f8
Mio Bambino <DarkMio@users.noreply.github.com>
2016-12-13 15:36:38 +0100

    Update Program.cs

--- Decompiler/Program.cs
+++ Decompiler/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using ValveResourceFormat;
 using ValveResourceFormat.ResourceTypes;
