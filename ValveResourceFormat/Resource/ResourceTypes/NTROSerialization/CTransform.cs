ffc13bfbfcf2cb2285fe1aaaf073cd0bcc27df2a
Pavel Djundik <github@xpaw.me>
2018-01-23 00:31:54 +0200

    Changes types in CTransform

--- ValveResourceFormat/Resource/ResourceTypes/NTROSerialization/CTransform.cs
+++ ValveResourceFormat/Resource/ResourceTypes/NTROSerialization/CTransform.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ValveResourceFormat.ResourceTypes.NTROSerialization
 {
@@ -7,10 +8,9 @@ namespace ValveResourceFormat.ResourceTypes.NTROSerialization
     /// </summary>
     public class CTransform
     {
-        /// <summary>
-        /// Gets the matrix values.
-        /// </summary>
-        public float[] Values { get; }
+        public Quaternion Q { get; }
+        public Vector3 P { get; }
+        public float Scale { get; } // TODO: Unconfirmed? resourceinfo appears to not print this field at all
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CTransform"/> class.
@@ -25,23 +25,15 @@ namespace ValveResourceFormat.ResourceTypes.NTROSerialization
         /// <param name="field7">Eighth item of the matrix.</param>
         public CTransform(float field0, float field1, float field2, float field3, float field4, float field5, float field6, float field7)
         {
-            Values = new[]
-            {
-                field0,
-                field1,
-                field2,
-                field3,
-                field4,
-                field5,
-                field6,
-                field7,
-            };
+            Q = new Quaternion(field4, field5, field6, field7);
+            P = new Vector3(field0, field1, field2);
+            Scale = field3;
         }
 
         public override string ToString()
         {
             // http://stackoverflow.com/a/15085178/2200891
-            return string.Format("q={{{0:F2}, {1:F2}, {2:F2}; w={3}}} p={{{4:F2}, {5:F2}, {6}}}", Values[4], Values[5], Values[6], Values[7].ToString("F2"), Values[0], Values[1], Values[2].ToString("F2"));
+            return string.Format("q={{{0:F2}, {1:F2}, {2:F2}; w={3}}} p={{{4:F2}, {5:F2}, {6}}}", Q.X, Q.Y, Q.Z, Q.W.ToString("F2"), P.X, P.Y, P.Z.ToString("F2"));
         }
     }
 }
