0fec32d248d02d0bcdd9074148cb4e1c218537c3
Pavel Djundik <xPaw@users.noreply.github.com>
2019-07-05 10:08:09 +0300

    Fix calling vrf_decompiler from another working directory

--- Decompiler/vrf_decompiler.bat
+++ Decompiler/vrf_decompiler.bat
@@ -3,5 +3,5 @@
 WHERE dotnet >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 ECHO You need to install .NET Core from https://dot.net && EXIT /B 1
 
-dotnet Decompiler.dll %*
+dotnet %~dp0Decompiler.dll %*
 EXIT /B %errorlevel%
