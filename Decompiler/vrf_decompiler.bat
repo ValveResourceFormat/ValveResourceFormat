008fade6b89960d48160eb66c925c443ea264bd5
Pavel Djundik <xPaw@users.noreply.github.com>
2020-02-07 14:16:13 +0200

    Publish single file, self contained decompiler builds

--- Decompiler/vrf_decompiler.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-WHERE dotnet >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 ECHO You need to install .NET Core from https://dot.net && EXIT /B 1
-
-dotnet %~dp0Decompiler.dll %*
-EXIT /B %errorlevel%
