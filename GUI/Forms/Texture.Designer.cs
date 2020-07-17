e3d91ae1ebdf5298122c9f2fefded7b88bed3011
Pavel Djundik <xPaw@users.noreply.github.com>
2020-07-17 12:16:56 +0300

    Add checkered background in texture viewer

--- GUI/Forms/Texture.Designer.cs
+++ GUI/Forms/Texture.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace GUI.Forms
+using System.Windows.Forms;
+
+namespace GUI.Forms
 {
     partial class Texture
     {
@@ -47,6 +49,8 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.BackgroundImageLayout = ImageLayout.Tile;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("checkered.Image")));
             // 
             // contextMenuStrip1
             // 
