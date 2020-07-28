d52aa8ba68291839e8a3b26f8a7cbeac26a79a76
Pavel Djundik <xPaw@users.noreply.github.com>
2020-07-28 13:47:21 +0300

    Initial implementation for searching gameinfo files and loading pak01 vpks

--- GUI/Utils/AdvancedGuiFileLoader.cs
+++ GUI/Utils/AdvancedGuiFileLoader.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using SteamDatabase.ValvePak;
+using ValveKeyValue;
 using ValveResourceFormat;
 using ValveResourceFormat.IO;
 
@@ -13,8 +14,10 @@ namespace GUI.Utils
     public class AdvancedGuiFileLoader : IFileLoader
     {
         private static readonly Dictionary<string, Package> CachedPackages = new Dictionary<string, Package>();
+        private readonly List<Package> CurrentGamePackages = new List<Package>();
         private readonly Dictionary<string, Resource> CachedResources = new Dictionary<string, Resource>();
         private readonly VrfGuiContext GuiContext;
+        private bool GamePackagesScanned;
 
         public AdvancedGuiFileLoader(VrfGuiContext guiContext)
         {
@@ -71,8 +74,14 @@ namespace GUI.Utils
                 return resource;
             }
 
+            if (!GamePackagesScanned)
+            {
+                GamePackagesScanned = true;
+                FindAndLoadSearchPaths();
+            }
+
             var paths = Settings.Config.GameSearchPaths.ToList();
-            var packages = new List<Package>();
+            var packages = CurrentGamePackages.ToList();
 
             foreach (var searchPath in paths.Where(searchPath => searchPath.EndsWith(".vpk")).ToList())
             {
@@ -141,6 +150,102 @@ namespace GUI.Utils
             CachedResources[file] = resource;
 
             return resource;
+        }
+
+        private void FindAndLoadSearchPaths()
+        {
+            var gameinfoPath = GetCurrentGameInfoPath();
+
+            if (gameinfoPath == null)
+            {
+                return;
+            }
+
+            var folders = new List<string>();
+            var rootFolder = Path.GetDirectoryName(Path.GetDirectoryName(gameinfoPath));
+            KVObject gameInfo;
+
+            using (var stream = new FileStream(gameinfoPath, FileMode.Open, FileAccess.Read))
+            {
+                gameInfo = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize(stream);
+            }
+
+            Console.WriteLine($"Found \"{gameInfo["game"]}\" from \"{gameinfoPath}\"");
+
+            foreach (var searchPath in (IEnumerable<KVObject>)gameInfo["FileSystem"]["SearchPaths"])
+            {
+                if (searchPath.Name != "Game")
+                {
+                    continue;
+                }
+
+                folders.Add(Path.Combine(rootFolder, searchPath.Value.ToString()));
+            }
+
+            foreach (var folder in folders)
+            {
+                // Scan for vpks in folder, same logic as in source engine
+                for (var i = 1; i < 99; i++)
+                {
+                    var vpk = Path.Combine(folder, $"pak{i:D2}_dir.vpk");
+
+                    if (!File.Exists(vpk))
+                    {
+                        break;
+                    }
+
+                    if (GuiContext.FileName == vpk)
+                    {
+#if DEBUG_FILE_LOAD
+                        Console.WriteLine($"VPK \"{vpk}\" is the same we just opened, skipping");
+#endif
+                        continue;
+                    }
+
+                    if (Settings.Config.GameSearchPaths.Contains(vpk))
+                    {
+#if DEBUG_FILE_LOAD
+                        Console.WriteLine($"VPK \"{vpk}\" is already user-defined, skipping");
+#endif
+                        continue;
+                    }
+
+                    Console.WriteLine($"Preloading vpk \"{vpk}\"");
+
+                    var package = new Package();
+                    package.Read(vpk);
+                    CurrentGamePackages.Add(package);
+                }
+            }
+        }
+
+        private string GetCurrentGameInfoPath()
+        {
+            var directory = GuiContext.FileName;
+            var i = 10;
+
+            while (i-- > 0)
+            {
+                directory = Path.GetDirectoryName(directory);
+
+#if DEBUG_FILE_LOAD
+                Console.WriteLine($"Scanning \"{directory}\"");
+#endif
+
+                if (directory == null || Path.GetFileName(directory) == "steamapps")
+                {
+                    return null;
+                }
+
+                var gameinfoPath = Path.Combine(directory, "gameinfo.gi");
+
+                if (File.Exists(gameinfoPath))
+                {
+                    return gameinfoPath;
+                }
+            }
+
+            return null;
         }
 
         private static string FindResourcePath(IList<string> paths, string file, string currentFullPath = null)
