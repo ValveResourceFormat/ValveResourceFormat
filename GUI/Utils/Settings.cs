7f324ee7d713ff9cefce2de05262d696030153bb
Pavel Djundik <xPaw@users.noreply.github.com>
2020-12-28 12:54:37 +0200

    Default open dialog to steamapps/common folder for new users

--- GUI/Utils/Settings.cs
+++ GUI/Utils/Settings.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using ValveKeyValue;
 
 namespace GUI.Utils
@@ -45,6 +47,11 @@ namespace GUI.Utils
             {
                 Config.SavedCameras = new Dictionary<string, float[]>();
             }
+
+            if (string.IsNullOrEmpty(Config.OpenDirectory) && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Config.OpenDirectory = GetSteamPath();
+            }
         }
 
         public static void Save()
@@ -55,6 +62,27 @@ namespace GUI.Utils
             {
                 KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Serialize(stream, Config, nameof(ValveResourceFormat));
             }
+        }
+
+        private static string GetSteamPath()
+        {
+            try
+            {
+                var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Valve\\Steam") ??
+                          RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)
+                              .OpenSubKey("SOFTWARE\\Valve\\Steam");
+
+                if (key != null && key.GetValue("SteamPath") is string steamPath)
+                {
+                    return Path.GetFullPath(Path.Combine(steamPath, "steamapps", "common"));
+                }
+            }
+            catch
+            {
+                // Don't care about registry exceptions
+            }
+
+            return string.Empty;
         }
     }
 }
