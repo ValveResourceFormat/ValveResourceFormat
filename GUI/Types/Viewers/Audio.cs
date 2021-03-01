3d3b1052ea8a23e028369a8d14ccf2e7526e91be
Pavel Djundik <xPaw@users.noreply.github.com>
2021-03-01 10:39:15 +0200

    Detecting mp3 is complicated, so just check for extension

--- GUI/Types/Viewers/Audio.cs
+++ GUI/Types/Viewers/Audio.cs
@@ -13,7 +13,7 @@ namespace GUI.Types.Viewers
         public static bool IsAccepted(uint magic, string fileName)
         {
             return (magic == 0x46464952 /* RIFF */ && fileName.EndsWith(".wav", StringComparison.InvariantCultureIgnoreCase)) ||
-                    (magic << 8 == 0x33444900 /* ID3 */ && fileName.EndsWith(".mp3", StringComparison.InvariantCultureIgnoreCase));
+                    fileName.EndsWith(".mp3", StringComparison.InvariantCultureIgnoreCase);
         }
 
         public TabPage Create(VrfGuiContext vrfGuiContext, byte[] input)
