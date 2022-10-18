aafd2dd9ed0ecc7d5cba4cdd7a9bfb8a1792043f
kristiker <kristik06@outlook.com>
2022-10-18 16:36:08 +0200

    .vtex formatting

--- ValveResourceFormat/IO/TextureExtract.cs
+++ ValveResourceFormat/IO/TextureExtract.cs
@@ -167,7 +167,12 @@ public sealed class TextureExtract
 
     private string GetInputFileNameForVtex()
     {
-        return GetImageFileName();
+        if (spriteSheetData is not null)
+        {
+            return GetMksFileName().Replace(Path.DirectorySeparatorChar, '/');
+        }
+
+        return GetImageFileName().Replace(Path.DirectorySeparatorChar, '/');
     }
 
     public string ToValveTexture()
@@ -175,55 +180,53 @@ public sealed class TextureExtract
         var inputTextureFileName = GetInputFileNameForVtex();
         var outputFormat = texture.Format.ToString();
 
-        var template = @"<!-- dmx encoding keyvalues2_noids 1 format vtex 1 -->
-""CDmeVtex""
-{
-	""m_inputTextureArray"" ""element_array"" 
-	[
-		""CDmeInputTexture""
-		{
-			""m_name"" ""string"" ""InputTexture0""
-			""m_fileName"" ""string"" ""{0}""
-			""m_colorSpace"" ""string"" ""srgb""
-			""m_typeString"" ""string"" ""2D""
-			""m_imageProcessorArray"" ""element_array"" 
-			[
-				""CDmeImageProcessor""
-				{
-					""m_algorithm"" ""string"" ""None""
-					""m_stringArg"" ""string"" """"
-					""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
-				}
-			]
-		}
-	]
-	""m_outputTypeString"" ""string"" ""2D""
-	""m_outputFormat"" ""string"" ""{1}""
-	""m_outputClearColor"" ""vector4"" ""0 0 0 0""
-	""m_nOutputMinDimension"" ""int"" ""0""
-	""m_nOutputMaxDimension"" ""int"" ""0""
-	""m_textureOutputChannelArray"" ""element_array"" 
-	[
-		""CDmeTextureOutputChannel""
-		{
-			""m_inputTextureArray"" ""string_array"" [ ""InputTexture0"" ]
-			""m_srcChannels"" ""string"" ""rgba""
-			""m_dstChannels"" ""string"" ""rgba""
-			""m_mipAlgorithm"" ""CDmeImageProcessor""
-			{
-				""m_algorithm"" ""string"" ""Box""
-				""m_stringArg"" ""string"" """"
-				""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""
-			}
-			""m_outputColorSpace"" ""string"" ""srgb""
-		}
-	]
-	""m_vClamp"" ""vector3"" ""0 0 0""
-	""m_bNoLod"" ""bool"" ""0""
-}";
-
-        return template;
-        //return string.Format(CultureInfo.InvariantCulture, template, inputTextureFileName, outputFormat);
+        return string.Join(Environment.NewLine,
+        "<!-- dmx encoding keyvalues2_noids 1 format vtex 1 -->",
+        @"""CDmeVtex""",
+        @"{",
+        @"    ""m_inputTextureArray"" ""element_array""",
+        @"    [",
+        @"        ""CDmeInputTexture""",
+        @"        {",
+        @"            ""m_name"" ""string"" ""InputTexture0""",
+        $@"            ""m_fileName"" ""string"" ""{inputTextureFileName}""",
+        @"            ""m_colorSpace"" ""string"" ""srgb""",
+        @"            ""m_typeString"" ""string"" ""2D""",
+        @"            ""m_imageProcessorArray"" ""element_array""",
+        @"            [",
+        @"                ""CDmeImageProcessor""",
+        @"                {",
+        @"                    ""m_algorithm"" ""string"" ""None""",
+        @"                    ""m_stringArg"" ""string"" """"",
+        @"                    ""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""",
+        @"                }",
+        @"            ]",
+        @"        }",
+        @"    ]",
+        @"    ""m_outputTypeString"" ""string"" ""2D""",
+        $@"    ""m_outputFormat"" ""string"" ""{outputFormat}""",
+        @"    ""m_outputClearColor"" ""vector4"" ""0 0 0 0""",
+        @"    ""m_nOutputMinDimension"" ""int"" ""0""",
+        @"    ""m_nOutputMaxDimension"" ""int"" ""0""",
+        @"    ""m_textureOutputChannelArray"" ""element_array"" ",
+        @"    [",
+        @"        ""CDmeTextureOutputChannel""",
+        @"        {",
+        @"            ""m_inputTextureArray"" ""string_array"" [ ""InputTexture0"" ]",
+        @"            ""m_srcChannels"" ""string"" ""rgba""",
+        @"            ""m_dstChannels"" ""string"" ""rgba""",
+        @"            ""m_mipAlgorithm"" ""CDmeImageProcessor""",
+        @"            {",
+        @"                ""m_algorithm"" ""string"" ""Box""",
+        @"                ""m_stringArg"" ""string"" """"",
+        @"                ""m_vFloat4Arg"" ""vector4"" ""0 0 0 0""",
+        @"            }",
+        @"            ""m_outputColorSpace"" ""string"" ""srgb""",
+        @"        }",
+        @"    ]",
+        @"    ""m_vClamp"" ""vector3"" ""0 0 0""",
+        @"    ""m_bNoLod"" ""bool"" """"",
+        @"}");
     }
 }
 
