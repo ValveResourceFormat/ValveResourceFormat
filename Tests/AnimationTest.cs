cef0d9c4df3275960ec429593f46b147a1cf7f5b
Pavel Djundik <xPaw@users.noreply.github.com>
2022-11-16 12:22:52 +0200

    Add various files to tests

---
+++ Tests/AnimationTest.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using ValveResourceFormat;
+using ValveResourceFormat.ResourceTypes;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AnimationTest
+    {
+        [Test]
+        public void TestEmbeddedAnimations()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "box_creature_ik_model.vmdl_c");
+            using var resource = new Resource
+            {
+                FileName = file,
+            };
+            resource.Read(file);
+
+            var model = (Model)resource.DataBlock;
+
+            var animGroupPaths = model.GetReferencedAnimationGroupNames();
+            var animations = model.GetEmbeddedAnimations().ToList();
+
+            Assert.That(animGroupPaths.Count, Is.EqualTo(0));
+            Assert.That(animations.Count, Is.EqualTo(3));
+
+            Assert.That(animations[0].Name, Is.EqualTo("ref_pose"));
+            Assert.That(animations[0].Fps, Is.EqualTo(30));
+            Assert.That(animations[0].FrameCount, Is.EqualTo(1));
+            Assert.That(animations[0].Frames.Count, Is.EqualTo(animations[0].FrameCount));
+
+            Assert.That(animations[1].Name, Is.EqualTo("box_creature_leggy_idle"));
+            Assert.That(animations[1].Fps, Is.EqualTo(30));
+            Assert.That(animations[1].FrameCount, Is.EqualTo(49));
+            Assert.That(animations[1].Frames.Count, Is.EqualTo(animations[1].FrameCount));
+
+            Assert.That(animations[2].Name, Is.EqualTo("box_creature_leggy_walk"));
+            Assert.That(animations[2].Fps, Is.EqualTo(30));
+            Assert.That(animations[2].FrameCount, Is.EqualTo(25));
+            Assert.That(animations[2].Frames.Count, Is.EqualTo(animations[2].FrameCount));
+        }
+    }
+}
