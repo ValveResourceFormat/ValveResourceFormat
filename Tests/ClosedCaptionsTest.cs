d80203b473ed622b902c8f9a67ca49fbdc316d33
Pavel Djundik <xPaw@users.noreply.github.com>
2022-11-18 23:25:07 +0200

    Add test for ClosedCaptions

---
+++ Tests/ClosedCaptionsTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using ValveResourceFormat.ClosedCaptions;
+
+namespace Tests
+{
+    public class ClosedCaptionsTest
+    {
+        [Test]
+        public void ParseClosedCaptions()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "subtitles_announcer_killing_spree_english.dat");
+
+            var captions = new ClosedCaptions();
+            captions.Read(file);
+
+            Assert.That(captions.Captions.Count, Is.EqualTo(840));
+
+            var caption = captions.Captions[839];
+            Assert.That(caption.Blocknum, Is.EqualTo(4));
+            Assert.That(caption.Hash, Is.EqualTo(3873743860));
+            Assert.That(caption.HashText, Is.EqualTo(3502107501));
+            Assert.That(caption.Length, Is.EqualTo(16));
+            Assert.That(caption.Offset, Is.EqualTo(2086));
+            Assert.That(caption.Text, Is.EqualTo("Ownage!"));
+
+            var captionByCrc = captions["announcer_killing_spree_announcer_ownage_01"];
+            Assert.That(captionByCrc, Is.EqualTo(caption));
+
+            var i = 0;
+            var found = false;
+
+            foreach (var captionInLoop in captions)
+            {
+                i++;
+
+                if (captionInLoop.Hash == caption.Hash)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            Assert.True(found);
+            Assert.That(i, Is.EqualTo(840));
+        }
+    }
+}
