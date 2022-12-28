67ac314b7e6a9d38da0163d7740041abbd008636
Pavel Djundik <xPaw@users.noreply.github.com>
2022-12-28 16:19:47 +0200

    Add fgd parser code

---
+++ Misc/VrfFgdParser/Program.cs
@@ -0,0 +1,149 @@
+using System.Text;
+using Sledge.Formats.GameData;
+using Sledge.Formats.GameData.Objects;
+
+var allIcons = new SortedDictionary<string, string>();
+var allProperties = new HashSet<string>();
+var baseIcons = new Dictionary<string, string>();
+
+foreach (var file in Directory.EnumerateFiles("fgds", "*.fgd", SearchOption.AllDirectories))
+{
+    var isSource2 = File.Exists(Path.Join(Path.GetDirectoryName(file), "gameinfo.gi"));
+
+    Console.WriteLine();
+    Console.Write("Parsing ");
+    Console.Write(file);
+    Console.WriteLine(isSource2 ? string.Empty : " (not Source 2)");
+
+    GameDefinition fgd;
+
+    try
+    {
+        fgd = FgdFormatter.ReadFile(file);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        continue;
+    }
+
+    foreach (var _class in fgd.Classes)
+    {
+        foreach (var property in _class.Properties)
+        {
+            allProperties.Add(property.Name.ToLowerInvariant());
+        }
+
+        foreach (var behaviour in _class.Behaviours)
+        {
+            string? value = null;
+
+            // TODO: Skip for !isSource2
+
+            if (behaviour.Name == "iconsprite" && behaviour.Values.Count > 0)
+            {
+                value = behaviour.Values[0];
+
+                if (!value.StartsWith("materials/"))
+                {
+                    value = "materials/" + value;
+                }
+
+                if (value.EndsWith(".vmt"))
+                {
+                    value = value[..^4] + ".vmat";
+                }
+
+                if (!value.EndsWith(".vmat"))
+                {
+                    value += ".vmat";
+                }
+            }
+
+            if ((behaviour.Name == "studio" || behaviour.Name == "editormodel") && behaviour.Values.Count > 0)
+            {
+                // TODO: get model property
+                value = behaviour.Values[0];
+
+                if (!value.StartsWith("models/"))
+                {
+                    value = "models/" + value;
+                }
+
+                if (value.EndsWith(".mdl"))
+                {
+                    value = value[..^4] + ".vmdl";
+                }
+
+                if (!value.EndsWith(".vmdl"))
+                {
+                    value += ".vmdl";
+                }
+            }
+
+            if (value == null && _class.ClassType == ClassType.BaseClass)
+            {
+                foreach (var baseClass in _class.BaseClasses)
+                {
+                    if (baseIcons.TryGetValue(baseClass, out value))
+                    {
+                        Console.WriteLine($"Found {_class.Name} found base icon from {baseClass}");
+                        break;
+                    }
+                }
+            }
+
+            if (value != null)
+            {
+                IDictionary<string, string> icons = _class.ClassType == ClassType.BaseClass ? baseIcons : allIcons;
+
+                if (icons.TryGetValue(_class.Name, out var existingValue))
+                {
+                    if (existingValue != value)
+                    {
+                        Console.WriteLine($"Found {_class.Name} with different value: {value} => {existingValue}");
+                        continue;
+                    }
+
+                    continue;
+                }
+
+                icons[_class.Name] = value;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Found {allIcons.Count} entities with icons");
+
+var iconsString = new StringBuilder();
+
+foreach (var icon in allIcons)
+{
+    iconsString.Append('"');
+    iconsString.Append(icon.Key);
+    iconsString.Append('"');
+    iconsString.Append(" => ");
+    iconsString.Append('"');
+    iconsString.Append(icon.Value);
+    iconsString.Append('"');
+    iconsString.Append(',');
+    iconsString.AppendLine();
+}
+
+File.WriteAllText("icons.txt", iconsString.ToString());
+
+Console.WriteLine($"Found {allProperties.Count} unique properties");
+
+var propertiesString = new StringBuilder();
+
+foreach (var property in allProperties.OrderBy(x => x))
+{
+    propertiesString.Append('"');
+    propertiesString.Append(property);
+    propertiesString.Append('"');
+    propertiesString.Append(',');
+    propertiesString.AppendLine();
+}
+
+File.WriteAllText("properties.txt", propertiesString.ToString());
