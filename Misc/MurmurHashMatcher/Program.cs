61b7c0a9093cdf10e6badeec55e7a7ef37bcbc1e
Pavel Djundik <xPaw@users.noreply.github.com>
2022-12-28 21:40:54 +0200

    Allow bruteforcing murmur hashes over a strings.txt file

--- Misc/MurmurHashMatcher/Program.cs
+++ Misc/MurmurHashMatcher/Program.cs
@@ -1,14 +1,11 @@
 using System.Globalization;
-using System.Linq;
 using System.Text;
 using ValveResourceFormat.ThirdParty;
 using ValveResourceFormat.Utils;
 
-if (args?.Length < 1 || !Directory.Exists(args![0]))
-{
-    Console.Error.WriteLine($"Usage: ./program <path to folder to find vmaps in>");
-    return 1;
-}
+Console.Error.WriteLine($"Usage: ./program <path to folder to find vmaps in>");
+Console.Error.WriteLine("Alternatively, create strings.txt with one string per line.");
+Console.WriteLine();
 
 // Generate with Decompiler:
 // .\Decompiler.exe -i "Steam\steamapps\common" --threads 10 --stats --dump_unknown_entity_keys --vpk_extensions "vents_c" --recursive --recursive_vpk
@@ -18,39 +15,61 @@ var unknownKeys = File.ReadAllLines("unknown_keys.txt")
     .ToHashSet();
 var foundKeys = new List<string>();
 
-foreach (var file in Directory.EnumerateFiles(args[0], "*.vmap", SearchOption.AllDirectories))
+if (File.Exists("strings.txt"))
 {
-    Console.WriteLine($"Scanning {file}");
+    Console.WriteLine($"Scanning strings.txt");
 
-    var bytes = File.ReadAllBytes(file).AsSpan();
+    var strings = File.ReadAllLines("strings.txt");
 
-    while (!bytes.IsEmpty)
+    foreach (var str in strings)
     {
-        // This is a very rudimentary bruteforce over vmap files to find all the strings in it
-        var position = bytes.IndexOf((byte)0);
-
-        if (position == -1)
-        {
-            break;
-        }
-
-        var strBytes = bytes[0..position];
-        var str = Encoding.UTF8.GetString(strBytes);
-        var hash = MurmurHash2.Hash(strBytes.ToArray(), StringToken.MURMUR2SEED);
+        var strLower = str.ToLowerInvariant();
+        var hash = MurmurHash2.Hash(strLower, StringToken.MURMUR2SEED);
 
         if (unknownKeys.Remove(hash))
         {
-            foundKeys.Add(str);
-            Console.WriteLine($"Found string: {str}");
+            foundKeys.Add(strLower);
+            Console.WriteLine($"Found string: {strLower}");
         }
+    }
+}
 
-        bytes = bytes[(position + 1)..];
+if (args?.Length > 0 && Directory.Exists(args![0]))
+{
+    foreach (var file in Directory.EnumerateFiles(args[0], "*.vmap", SearchOption.AllDirectories))
+    {
+        Console.WriteLine($"Scanning {file}");
+
+        var bytes = File.ReadAllBytes(file).AsSpan();
+
+        while (!bytes.IsEmpty)
+        {
+            // This is a very rudimentary bruteforce over vmap files to find all the strings in it
+            var position = bytes.IndexOf((byte)0);
+
+            if (position == -1)
+            {
+                break;
+            }
+
+            var strBytes = bytes[0..position];
+            var str = Encoding.UTF8.GetString(strBytes).ToLowerInvariant();
+            var hash = MurmurHash2.Hash(str, StringToken.MURMUR2SEED);
+
+            if (unknownKeys.Remove(hash))
+            {
+                foundKeys.Add(str);
+                Console.WriteLine($"Found string: {str}");
+            }
+
+            bytes = bytes[(position + 1)..];
+        }
     }
 }
 
 if (unknownKeys.Count > 0)
 {
-    Console.WriteLine($"Did not find {unknownKeys.Count} hashed:");
+    Console.WriteLine($"Did not find {unknownKeys.Count} hashes:");
     Console.WriteLine();
 
     foreach (var hash in unknownKeys)
