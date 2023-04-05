bf8bd52e5e33663a1640add49b1ffc39d8232b84
Pavel Djundik <xPaw@users.noreply.github.com>
2023-04-05 10:54:47 +0300

    Revert "Enable depth testing for phys nodes"
    This reverts commit d9bb9536d08bbfd2ab2cc0fd3465dd0c41b10275.

--- GUI/Types/Renderer/PhysSceneNode.cs
+++ GUI/Types/Renderer/PhysSceneNode.cs
@@ -369,13 +369,13 @@ namespace GUI.Types.Renderer
             GL.UseProgram(shader.Program);
 
             GL.UniformMatrix4(shader.GetUniformLocation("uProjectionViewMatrix"), false, ref viewProjectionMatrix);
-            GL.Enable(EnableCap.DepthTest);
+            GL.DepthMask(false);
 
             GL.BindVertexArray(vaoHandle);
             GL.DrawElements(PrimitiveType.Lines, indexCount, DrawElementsType.UnsignedInt, 0);
             GL.BindVertexArray(0);
 
-            GL.Disable(EnableCap.DepthTest);
+            GL.DepthMask(true);
         }
 
         public override void Update(Scene.UpdateContext context)
