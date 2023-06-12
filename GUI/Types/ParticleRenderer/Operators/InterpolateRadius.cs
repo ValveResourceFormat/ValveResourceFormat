4dc196698824a058e2b5f3b2dc6fb969a77c0b3c
Perryvw <perry.wesel@gmail.com>
2023-06-12 22:26:19 +0200

    Add radius interpolate bias parameter

--- GUI/Types/ParticleRenderer/Operators/InterpolateRadius.cs
+++ GUI/Types/ParticleRenderer/Operators/InterpolateRadius.cs
@@ -7,8 +7,9 @@ namespace GUI.Types.ParticleRenderer.Operators
     {
         private readonly float startTime;
         private readonly float endTime = 1;
-        private readonly float startScale = 1;
-        private readonly float endScale = 1;
+        private readonly INumberProvider startScale = new LiteralNumberProvider(1);
+        private readonly INumberProvider endScale = new LiteralNumberProvider(1);
+        private readonly INumberProvider bias = new LiteralNumberProvider(0);
 
         public InterpolateRadius(IKeyValueCollection keyValues)
         {
@@ -24,12 +25,17 @@ namespace GUI.Types.ParticleRenderer.Operators
 
             if (keyValues.ContainsKey("m_flStartScale"))
             {
-                startScale = keyValues.GetFloatProperty("m_flStartScale");
+                startScale = keyValues.GetNumberProvider("m_flStartScale");
             }
 
             if (keyValues.ContainsKey("m_flEndScale"))
             {
-                endScale = keyValues.GetFloatProperty("m_flEndScale");
+                endScale = keyValues.GetNumberProvider("m_flEndScale");
+            }
+
+            if (keyValues.ContainsKey("m_flBias"))
+            {
+                bias = keyValues.GetNumberProvider("m_flBias");
             }
         }
 
@@ -41,10 +47,11 @@ namespace GUI.Types.ParticleRenderer.Operators
 
                 if (time >= startTime && time <= endTime)
                 {
-                    var t = (time - startTime) / (endTime - startTime);
-                    var radiusScale = (startScale * (1 - t)) + (endScale * t);
+                    double t = (time - startTime) / (endTime - startTime);
+                    t = Math.Pow(t, 1.0 - bias.NextNumber()); // apply bias to timescale
+                    var radiusScale = (startScale.NextNumber() * (1 - t)) + (endScale.NextNumber() * t);
 
-                    particles[i].Radius = particles[i].ConstantRadius * radiusScale;
+                    particles[i].Radius = particles[i].ConstantRadius * (float)radiusScale;
                 }
             }
         }
