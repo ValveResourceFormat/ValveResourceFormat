52a84fe7464b43bb6478d0f5a998e4b4e97918eb
Purrspctiv <36551033+Purrspctiv@users.noreply.github.com>
2023-07-06 16:57:43 -0400

    Use actual PBR equation from S2
    Note that this no longer includes a division by pi, which would make this physically inaccurate. However, this is indeed accurate to Source 2. Even in the light precalculations theres no division by pi. This might've changed in deskjob, but this hasn't been tested.

--- GUI/Types/Renderer/Shaders/common/pbr.glsl
+++ GUI/Types/Renderer/Shaders/common/pbr.glsl
@@ -3,62 +3,61 @@
 // Normal Distribution function --------------------------------------
 float D_GGX(float NoH, float roughness)
 {
-	float alpha = roughness * roughness;
-	float alpha2 = alpha * alpha;
-	float denom = NoH * NoH * (alpha2 - 1.0) + 1.0;
-	return (alpha2)/(PI * denom*denom); 
+	float alpha = pow2(roughness);
+	float denom = pow2(NoH) * (pow2(alpha) - 1.0) + 1.0;
+	return pow2(alpha / denom);
 }
 
-// Geometric Shadowing function --------------------------------------
+// Geometric Shadowing visibility function --------------------------------------
 float G_SchlicksmithGGX(float NoL, float NoV, float roughness)
 {
-	float r = (roughness + 1.0);
-	float k = (r * r) / 8.0;
-	float GL = NoL / (NoL * (1.0 - k) + k);
-	float GV = NoV / (NoV * (1.0 - k) + k);
-	return GL * GV;
+    float VisRough = pow2(roughness + 1.0) / 8.0;
+    float SchlickVisL = NoL * (1.0 - VisRough) + VisRough;
+    float SchlickVisV = NoV * (1.0 - VisRough) + VisRough;
+    float Vis = 1.0 / (4.0 * SchlickVisL * SchlickVisV);
+    return Vis;
 }
 
 // Fresnel function ----------------------------------------------------
-float F_Schlick(float f0, float f90, float VoH) {
-    return f0 + (f90 - f0) * pow(1.0 - VoH, 5.0);
+float F_Schlick(float F0, float F90, float VoH)
+{
+    return mix(F0, F90, pow(1.0 - VoH, 5.0));
 }
 
 vec3 F_Schlick(float cosTheta, vec3 F0)
 {
-	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-vec3 F_SchlickR(float cosTheta, vec3 F0, float roughness)
-{
-	return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(1.0 - cosTheta, 5.0);
+	return mix(F0, vec3(1.0), pow(1.0 - cosTheta, 5.0));
 }
 
-vec3 specularContribution(vec3 L, vec3 V, vec3 N, vec3 F0, vec3 albedo, float metallic, float roughness)
+vec3 specularContribution(vec3 L, vec3 V, vec3 N, vec3 F0, vec3 specularColor, float roughness)
 {
 	// Precalculate vectors and dot products	
-	vec3 H = normalize (V + L);
-	float NoH = clamp(dot(N, H), 0.0, 1.0);
-	float NoV = clamp(dot(N, V), 0.0, 1.0);
-	float NoL = clamp(dot(N, L), 0.0, 1.0);
+	float NoL = saturate( dot(N, L) );
 
-	vec3 color = vec3(0.0);
+	if (NoL > 0.0)
+    {
+	    vec3 H = normalize(V + L);
+	    float NoH = saturate( dot(N, H) );
+	    float NoV = saturate( dot(N, V) );
+	    float VoH = ClampToPositive(dot(L, H));
 
-	if (NoL > 0.0) {
-		// D = Normal distribution (Distribution of the microfacets)
+        // D = Normal distribution (Distribution of the microfacets)
 		float D = D_GGX(NoH, roughness); 
 		// G = Geometric shadowing term (Microfacets shadowing)
 		float G = G_SchlicksmithGGX(NoL, NoV, roughness);
 		// F = Fresnel factor (Reflectance depending on angle of incidence)
-		vec3 F = F_Schlick(NoV, F0);
-		vec3 spec = D * F * G / (4.0 * NoL * NoV + 0.001);
-		vec3 kD = (vec3(1.0) - F) * (1.0 - metallic);			
-		color += (kD * albedo / PI + spec) * NoL;
+		vec3 F = F_Schlick(VoH, F0);
+
+        return vec3(D * G * NoL) * F;
 	}
 
-	return color;
+	return vec3(0.0);
 }
 
-vec3 diffuseLobe(vec3 diffuse)
+float diffuseLobe(float NoL, float roughness)
 {
-    return diffuse * (1.0 / PI);
+    // Pi is never factored into any lighting calculations in Source 2, for some reason (as of HLA)
+    float diffuseExponent = (1.0 - roughness) * 0.8 + 0.6;
+    return pow(NoL, diffuseExponent) * ((diffuseExponent + 1.0) / 2.0);
+    //return diffuse * (1.0 / PI);
 }
