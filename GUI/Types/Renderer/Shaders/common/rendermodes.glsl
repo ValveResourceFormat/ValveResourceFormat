885b6ba97ac04163b6839f3d5137a366d1f89d28
Purrspctiv <36551033+Purrspctiv@users.noreply.github.com>
2023-07-08 17:09:08 -0400

    More accurate Fullbright
    - Also fixed crash when glass is loaded

--- GUI/Types/Renderer/Shaders/common/rendermodes.glsl
+++ GUI/Types/Renderer/Shaders/common/rendermodes.glsl
@@ -69,5 +69,20 @@
 #elif defined(spritecard)
     const vec3 shaderIdColor = vec3(240, 240, 0);
 #else
-    const vec3 shaderIdColor = vec3(0);
+    const vec3 shaderIdColor = vec3(0, 255, 0);
 #endif
+
+
+vec3 CalculateFullbrightLighting(vec3 albedo, vec3 normal, vec3 viewVector)
+{
+    float flFakeDiffuseLighting = saturate(dot(normal, -viewVector)) * 0.7 + 0.3;
+    vec3 vReflectionDirWs = reflect(viewVector, normal);
+    float flFakeSpecularLighting = pow2(pow2(saturate(dot(-viewVector, vReflectionDirWs)))) * 0.05;
+
+    float XtraLight1 = dot(vec3(0.6, 0.4, 1.0), pow2(saturate(normal)));
+    float XtraLight2 = dot(vec3(0.6, 0.4, 0.2), pow2(saturate(-normal)));
+    float xtraLight = XtraLight1 + XtraLight2;
+    
+    //return XtraLightDiffuse * albedo * flFakeDiffuseLighting + flFakeSpecularLighting;
+    return xtraLight * albedo * flFakeDiffuseLighting + flFakeSpecularLighting;
+}
