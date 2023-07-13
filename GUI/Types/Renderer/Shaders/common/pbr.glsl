97d18f09a11a7f53d9dcc7b1f4ee2e4de41325dd
Purrspctiv <36551033+Purrspctiv@users.noreply.github.com>
2023-07-13 01:19:34 -0400

    Cloth Shading, Retroreflectivity
    Numerous changes to PBR shading, including support for cloth shading and retroreflectivity.

--- GUI/Types/Renderer/Shaders/common/pbr.glsl
+++ GUI/Types/Renderer/Shaders/common/pbr.glsl
@@ -1,5 +1,16 @@
 #define PI 3.1415926535897932384626433832795
 
+
+float diffuseLobe(float NoL, float roughness)
+{
+    // Pi is never factored into any lighting calculations in Source 2, for some reason (as of HLA)
+    float diffuseExponent = (1.0 - roughness) * 0.8 + 0.6;
+    return pow(NoL, diffuseExponent) * ((diffuseExponent + 1.0) / 2.0);
+    //return diffuse * (1.0 / PI);
+}
+
+
+
 // Normal Distribution function --------------------------------------
 float D_GGX(float NoH, float roughness)
 {
@@ -8,8 +19,9 @@ float D_GGX(float NoH, float roughness)
 	return pow2(alpha / denom);
 }
 
+
 // Geometric Shadowing visibility function --------------------------------------
-float G_SchlicksmithGGX(float NoL, float NoV, float roughness)
+float G_SchlickSmithGGX(float NoL, float NoV, float roughness)
 {
     float VisRough = pow2(roughness + 1.0) / 8.0;
     float SchlickVisL = NoL * (1.0 - VisRough) + VisRough;
@@ -29,35 +41,83 @@ vec3 F_Schlick(float cosTheta, vec3 F0)
 	return mix(F0, vec3(1.0), pow(1.0 - cosTheta, 5.0));
 }
 
-vec3 specularContribution(vec3 L, vec3 V, vec3 N, vec3 F0, vec3 specularColor, float roughness)
+
+
+
+
+#if F_RETRO_REFLECTIVE == 1
+
+vec3 GetRetroReflectiveNormal(float retroReflectivity, vec3 L, vec3 V, vec3 N, vec3 H)
 {
-	// Precalculate vectors and dot products	
-	float NoL = saturate( dot(N, L) );
-
-	if (NoL > 0.0)
-    {
-	    vec3 H = normalize(V + L);
-	    float NoH = saturate( dot(N, H) );
-	    float NoV = saturate( dot(N, V) );
-	    float VoH = ClampToPositive(dot(L, H));
-
-        // D = Normal distribution (Distribution of the microfacets)
-		float D = D_GGX(NoH, roughness); 
-		// G = Geometric shadowing term (Microfacets shadowing)
-		float G = G_SchlicksmithGGX(NoL, NoV, roughness);
-		// F = Fresnel factor (Reflectance depending on angle of incidence)
-		vec3 F = F_Schlick(VoH, F0);
-
-        return vec3(D * G * NoL) * F;
-	}
-
-	return vec3(0.0);
+    vec3 retroReflectionVector = L - reflect(-V, N);
+    return normalize(mix(H, retroReflectionVector, retroReflectivity));
 }
 
-float diffuseLobe(float NoL, float roughness)
+#endif
+
+
+
+#if F_CLOTH_SHADING == 1
+
+// This isn't even based on GGX, lol
+float D_Cloth(float roughness, float NoH)
 {
-    // Pi is never factored into any lighting calculations in Source 2, for some reason (as of HLA)
-    float diffuseExponent = (1.0 - roughness) * 0.8 + 0.6;
-    return pow(NoL, diffuseExponent) * ((diffuseExponent + 1.0) / 2.0);
-    //return diffuse * (1.0 / PI);
+    float invRough = 1.0 / roughness;
+    return (invRough + 2.0) * pow(1.0 - pow2(NoH), invRough * 0.5);
+}
+
+float Vis_Cloth(float NoL, float NoV)
+{
+    float clampedNoV = min(NoV + 0.001, 1.0);
+    return 1.0 / (4.0 * (clampedNoV + NoL - NoL * clampedNoV));
+}
+
+vec3 SpecularCloth(float roughness, float NoL, float NoH, float NoV, float VoH, vec3 specularColor)
+{
+    float NDF = D_Cloth(roughness, NoH);
+    float Vis = Vis_Cloth(NoL, NoV);
+    vec3 Fresnel = F_Schlick(VoH, specularColor);
+
+    return vec3(NoL * NDF * Vis) * Fresnel;
+}
+#endif
+
+
+vec3 specularLighting(vec3 L, vec3 V, vec3 N, vec3 F0, vec3 specularColor, float roughness, vec4 extraParams)
+{
+	float NoL = saturate( dot(N, L) );
+
+	vec3 H = normalize(V + L);
+
+#if F_RETRO_REFLECTIVE == 1
+    H = GetRetroReflectiveNormal(extraParams.r, L, V, N, H);
+#endif
+
+	float NoH = saturate( dot(N, H) );
+	float NoV = saturate( dot(N, V) );
+	float VoH = ClampToPositive(dot(L, H));
+
+#if defined(vr_complex) && (F_CLOTH_SHADING == 1)
+    return SpecularCloth(roughness, NoL, NoH, NoV, VoH, specularColor);
+#endif
+    // D = Normal distribution (Distribution of the microfacets)
+	float NDF = D_GGX(NoH, roughness); 
+	// G = Geometric shadowing term (Microfacets shadowing)
+	float Vis = G_SchlickSmithGGX(NoL, NoV, roughness);
+	// F = Fresnel factor (Reflectance depending on angle of incidence)
+	vec3 F = F_Schlick(VoH, F0);
+
+#if (F_CLOTH_SHADING == 1)
+    // I'm not sure how they blend to the cloth shading, but I'm assuming it's just blending shading
+    float ClothNDF = D_Cloth(roughness, NoH);
+    float ClothVis = Vis_Cloth(NoL, NoV);
+
+    float clothMask = extraParams.z;
+
+    float blendedClothShading = mix(NDF * Vis, ClothNDF * ClothVis, clothMask);
+
+    return vec3(blendedClothShading * NoL) * F;
+#else
+    return vec3(NDF * Vis * NoL) * F;
+#endif
 }
