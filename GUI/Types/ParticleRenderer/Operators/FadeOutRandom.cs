e2e238d2e0ab22874317a2f441691474b5b3559f
Pavel Djundik <xPaw@users.noreply.github.com>
2023-07-30 16:46:56 +0300

    Implement m_flFadeBias for FadeOut

--- GUI/Types/ParticleRenderer/Operators/FadeOutRandom.cs
+++ GUI/Types/ParticleRenderer/Operators/FadeOutRandom.cs
@@ -7,6 +7,7 @@ namespace GUI.Types.ParticleRenderer.Operators
     {
         private readonly float fadeOutTimeMin = 0.25f;
         private readonly float fadeOutTimeMax = 0.25f;
+        private readonly float fadeBias = 0.5f;
         private readonly float randomExponent = 1f;
         private readonly bool proportional = true;
 
@@ -17,17 +18,30 @@ namespace GUI.Types.ParticleRenderer.Operators
             randomExponent = parse.Float("m_flFadeOutTimeExp", randomExponent);
             proportional = parse.Boolean("m_bProportional", proportional);
 
+            var bias = parse.Float("m_flFadeBias", fadeBias);
+
+            if (bias == 0.0f)
+            {
+                bias = 0.5f;
+            }
+
+            fadeBias = bias;
+
             // Other things that exist that don't seem to do anything:
             // m_bEaseInAndOut
-            // m_flFadeBias
         }
 
         public void Update(ParticleCollection particles, float frameTime, ParticleSystemRenderState particleSystemState)
         {
             foreach (ref var particle in particles.Current)
             {
-                // TODO: Consistent rng
-                var fadeOutTime = MathUtils.RandomWithExponentBetween(randomExponent, fadeOutTimeMin, fadeOutTimeMax);
+                var fadeOutTime = fadeOutTimeMin;
+
+                if (fadeOutTimeMin != fadeOutTimeMax)
+                {
+                    // TODO: Consistent rng
+                    fadeOutTime = MathUtils.RandomWithExponentBetween(randomExponent, fadeOutTimeMin, fadeOutTimeMax);
+                }
 
                 var timeLeft = proportional
                     ? 1.0f - particle.NormalizedAge
@@ -36,6 +50,12 @@ namespace GUI.Types.ParticleRenderer.Operators
                 if (timeLeft <= fadeOutTime)
                 {
                     var newAlpha = (timeLeft / fadeOutTime) * particle.GetInitialScalar(particles, ParticleField.Alpha);
+
+                    if (fadeBias != 0.5f)
+                    {
+                        newAlpha /= ((1f / fadeBias - 2f) * (1 - newAlpha) + 1f);
+                    }
+
                     particle.Alpha = newAlpha;
                 }
             }
