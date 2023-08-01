daf36d052c9afdbfc0b4fe43173231fe6c508fa9
Pavel Djundik <xPaw@users.noreply.github.com>
2023-08-01 14:20:30 +0300

    Move to math utils

--- GUI/Types/ParticleRenderer/ParticleFunction.cs
+++ GUI/Types/ParticleRenderer/ParticleFunction.cs
@@ -1,5 +1,4 @@
-using System;
-using OpenTK;
+using GUI.Utils;
 
 namespace GUI.Types.ParticleRenderer
 {
@@ -20,7 +19,7 @@ namespace GUI.Types.ParticleRenderer
         //float OpTimeScaleMin; // operator fade time scale min
         //float OpTimeScaleMax; // operator fade time scale max
 
-        public bool StrengthFastPath;
+        bool StrengthFastPath;
 
         public ParticleFunction(ParticleDefinitionParser parse)
         {
@@ -82,56 +81,9 @@ namespace GUI.Types.ParticleRenderer
                 time %= 1f;
             }
 
-            var strength = FadeInOut(OpStartFadeInTime, OpEndFadeInTime, OpStartFadeOutTime, OpEndFadeOutTime, time);
+            var strength = MathUtils.FadeInOut(OpStartFadeInTime, OpEndFadeInTime, OpStartFadeOutTime, OpEndFadeOutTime, time);
 
             return strength;
-        }
-
-        static float FadeInOut(float fadeInStart, float fadeInEnd, float fadeOutStart, float fadeOutEnd, float time)
-        {
-            if (fadeInStart > time) // started yet?
-            {
-                return 0f;
-            }
-
-            if (fadeOutEnd > 0f && fadeOutEnd < time) // timed out?
-            {
-                return 0f;
-            }
-
-            // handle out of order cases
-            fadeInEnd = MathF.Max(fadeInEnd, fadeInStart);
-            fadeOutStart = MathF.Max(fadeOutStart, fadeInEnd);
-            fadeOutEnd = MathF.Max(fadeOutEnd, fadeOutStart);
-
-            var strength = 1f;
-
-            if (fadeInEnd > time && fadeInEnd > fadeInStart)
-            {
-                strength = MathF.Min(strength, FLerp(0, 1, fadeInStart, fadeInEnd, time));
-            }
-
-            if (time > fadeOutStart && fadeOutEnd > fadeOutStart)
-            {
-                strength = MathF.Min(strength, FLerp(0f, 1f, fadeOutEnd, fadeOutStart, time));
-            }
-
-            return strength;
-        }
-
-        // 5-argument floating point linear interpolation.
-        // FLerp(f1,f2,i1,i2,x)=
-        //    f1 at x=i1
-        //    f2 at x=i2
-        //   smooth lerp between f1 and f2 at x>i1 and x<i2
-        //   extrapolation for x<i1 or x>i2
-        //
-        //   If you know a function f(x)'s value (f1) at position i1, and its value (f2) at position i2,
-        //   the function can be linearly interpolated with FLerp(f1,f2,i1,i2,x)
-        //    i2=i1 will cause a divide by zero.
-        static float FLerp(float f1, float f2, float i1, float i2, float x)
-        {
-            return f1 + (f2 - f1) * (x - i1) / (i2 - i1);
         }
     }
 }
--- GUI/Utils/MathUtils.cs
+++ GUI/Utils/MathUtils.cs
@@ -44,6 +44,21 @@ namespace GUI.Utils
             return Lerp(Remap(x, inputMin, inputMax), outputMin, outputMax);
         }
 
+        // 5-argument floating point linear interpolation.
+        // FLerp(f1,f2,i1,i2,x)=
+        //    f1 at x=i1
+        //    f2 at x=i2
+        //   smooth lerp between f1 and f2 at x>i1 and x<i2
+        //   extrapolation for x<i1 or x>i2
+        //
+        //   If you know a function f(x)'s value (f1) at position i1, and its value (f2) at position i2,
+        //   the function can be linearly interpolated with FLerp(f1,f2,i1,i2,x)
+        //    i2=i1 will cause a divide by zero.
+        public static float FLerp(float f1, float f2, float i1, float i2, float x)
+        {
+            return f1 + (f2 - f1) * (x - i1) / (i2 - i1);
+        }
+
         public static void MinMaxFixUp<T>(ref T min, ref T max) where T : INumber<T>
         {
             if (min > max)
@@ -72,6 +87,38 @@ namespace GUI.Utils
         public static float ToDegrees(float rad)
         {
             return rad * (180.0f / MathF.PI);
+        }
+
+        public static float FadeInOut(float fadeInStart, float fadeInEnd, float fadeOutStart, float fadeOutEnd, float time)
+        {
+            if (fadeInStart > time) // started yet?
+            {
+                return 0f;
+            }
+
+            if (fadeOutEnd > 0f && fadeOutEnd < time) // timed out?
+            {
+                return 0f;
+            }
+
+            // handle out of order cases
+            fadeInEnd = MathF.Max(fadeInEnd, fadeInStart);
+            fadeOutStart = MathF.Max(fadeOutStart, fadeInEnd);
+            fadeOutEnd = MathF.Max(fadeOutEnd, fadeOutStart);
+
+            var strength = 1f;
+
+            if (fadeInEnd > time && fadeInEnd > fadeInStart)
+            {
+                strength = MathF.Min(strength, FLerp(0, 1, fadeInStart, fadeInEnd, time));
+            }
+
+            if (time > fadeOutStart && fadeOutEnd > fadeOutStart)
+            {
+                strength = MathF.Min(strength, FLerp(0f, 1f, fadeOutEnd, fadeOutStart, time));
+            }
+
+            return strength;
         }
     }
 }
