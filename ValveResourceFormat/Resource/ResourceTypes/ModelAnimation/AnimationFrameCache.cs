71bac26e9757d008aaaa35c67a0a4319a8d6d83f
Pavel Djundik <xPaw@users.noreply.github.com>
2023-09-27 15:31:34 +0300

    Fix incorrect frame interpolation on animation restart

--- ValveResourceFormat/Resource/ResourceTypes/ModelAnimation/AnimationFrameCache.cs
+++ ValveResourceFormat/Resource/ResourceTypes/ModelAnimation/AnimationFrameCache.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Numerics;
 
 namespace ValveResourceFormat.ResourceTypes.ModelAnimation
@@ -36,15 +37,16 @@ namespace ValveResourceFormat.ResourceTypes.ModelAnimation
         {
             // Calculate the index of the current frame
             var frameIndex = (int)(time * anim.Fps) % anim.FrameCount;
-            var t = ((time * anim.Fps) - frameIndex) % 1;
             var nextFrameIndex = (frameIndex + 1) % anim.FrameCount;
+            var t = ((time * anim.Fps) - frameIndex) % 1;
 
             // Get current and next frame
             var frame1 = GetFrame(anim, frameIndex);
             var frame2 = GetFrame(anim, nextFrameIndex);
 
-            // TODO: frame1 gets overwritten when animation restarts causing a glitch in the animation
-            //Debug.Assert(frame1.FrameIndex == frameIndex);
+            // Make sure second GetFrame call didn't return incorrect instance
+            Debug.Assert(frame1.FrameIndex == frameIndex);
+            Debug.Assert(frame2.FrameIndex == nextFrameIndex);
 
             // Interpolate bone positions, angles and scale
             for (var i = 0; i < frame1.Bones.Length; i++)
@@ -76,7 +78,7 @@ namespace ValveResourceFormat.ResourceTypes.ModelAnimation
 
             // Only two frames are cached at a time to minimize memory usage, especially with Autoplay enabled
             Frame frame;
-            if (frameIndex > PrevFrame.FrameIndex)
+            if (frameIndex == (NextFrame.FrameIndex + 1) % anim.FrameCount)
             {
                 frame = PrevFrame;
                 PrevFrame = NextFrame;
