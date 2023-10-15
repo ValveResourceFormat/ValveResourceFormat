c540bd618ecc6a4fc8a9900a10d6259b2c97bbb4
Pavel Djundik <xPaw@users.noreply.github.com>
2023-10-15 20:26:43 +0300

    Add basic test for hitting gltf model exporter

---
+++ Tests/GltfExtractTest.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using ValveResourceFormat;
+using ValveResourceFormat.IO;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GltfExtractTest
+    {
+        [Test]
+        public void TestModel()
+        {
+            using var resource = new Resource();
+            var worldPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "box_creature_ik_model.vmdl_c");
+            resource.Read(worldPath);
+
+            var gltf = new GltfModelExporter(new NullFileLoader())
+            {
+                ExportMaterials = false,
+                ProgressReporter = new Progress<string>(progress => { }),
+            };
+            gltf.Export(resource, null);
+        }
+
+        [Test]
+        public void TestMesh()
+        {
+            using var resource = new Resource();
+            var worldPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "chen_weapon.vmesh_c");
+            resource.Read(worldPath);
+
+            var gltf = new GltfModelExporter(new NullFileLoader())
+            {
+                ExportMaterials = false,
+                ProgressReporter = new Progress<string>(progress => { }),
+            };
+            gltf.Export(resource, null);
+        }
+
+        [Test]
+        public void TestWorld()
+        {
+            using var resource = new Resource();
+            var worldPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "world.vwrld_c");
+            resource.Read(worldPath);
+
+            var gltf = new GltfModelExporter(new NullFileLoader())
+            {
+                ExportMaterials = false,
+                ProgressReporter = new Progress<string>(progress => { }),
+            };
+            gltf.Export(resource, null);
+        }
+    }
+}
