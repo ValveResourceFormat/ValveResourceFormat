ValveResourceFormat/ValveResourceFormat

commit 7c648ac6059e64058f50275b5b4ddff856b49da7

Embed brdf lut texture so files outside of game contexts render correctly; actually load brdf for model/mat viewer

GUI/Types/Renderer/GLSceneViewer.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 using GUI.Controls;
@@ -11,6 +12,7 @@ using GUI.Types.ParticleRenderer;
 using GUI.Types.Renderer.UniformBuffers;
 using GUI.Utils;
 using OpenTK.Graphics.OpenGL;
+using ValveResourceFormat;
 
 namespace GUI.Types.Renderer
 {
@@ -126,26 +128,30 @@ namespace GUI.Types.Renderer
             bufferSet = new List<IBlockBindableBuffer>(2) { viewBuffer, lightingBuffer };
         }
 
-        protected abstract void LoadScene();
-
-        protected abstract void OnPicked(object sender, PickingTexture.PickingResponse pixelInfo);
-
-        protected virtual void OnLoad(object sender, EventArgs e)
+        public virtual void PreSceneLoad()
         {
-            baseGrid = new InfiniteGrid(Scene);
-            selectedNodeRenderer = new(Scene);
+            const string vtexFileName = "ggx_integrate_brdf_lut_schlick.vtex_c";
 
-            Camera.SetViewportSize(GLControl.Width, GLControl.Height);
+            // prefer loading from game, fallback to embedded
+            var brdfLutResource = GuiContext.LoadFile("textures/dev/" + vtexFileName);
+            Stream stream = null;
+            if (brdfLutResource == null)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                stream = assembly.GetManifestResourceStream("GUI.Utils." + vtexFileName);
 
-            Camera.Picker = new PickingTexture(Scene.GuiContext, OnPicked);
+                brdfLutResource = new Resource() { FileName = vtexFileName };
+                brdfLutResource.Read(stream);
+            }
 
-            CreateBuffers();
+            // TODO: add annoying force clamp for lut
+            Scene.LightingInfo.Lightmaps["g_tBRDFLookup"] = GuiContext.MaterialLoader.LoadTexture(brdfLutResource);
+            brdfLutResource?.Dispose();
+        }
 
-            var timer = new Stopwatch();
-            timer.Start();
-            LoadScene();
-            timer.Stop();
-            Log.Debug(GetType().Name, $"Loading scene time: {timer.Elapsed}, shader variants: {GuiContext.ShaderLoader.ShaderCount}, materials: {GuiContext.MaterialLoader.MaterialCount}");
+        public virtual void PostSceneLoad()
+        {
+            Scene.CalculateEnvironmentMaps();
 
             if (Scene.AllNodes.Any() && this is not GLWorldViewer)
             {
@@ -180,6 +186,30 @@ namespace GUI.Types.Renderer
             dynamicOctreeRenderer = new OctreeDebugRenderer<SceneNode>(Scene.DynamicOctree, Scene.GuiContext, true);
 
             SetAvailableRenderModes();
+        }
+
+        protected abstract void LoadScene();
+
+        protected abstract void OnPicked(object sender, PickingTexture.PickingResponse pixelInfo);
+
+        protected virtual void OnLoad(object sender, EventArgs e)
+        {
+            baseGrid = new InfiniteGrid(Scene);
+            selectedNodeRenderer = new(Scene);
+
+            Camera.SetViewportSize(GLControl.Width, GLControl.Height);
+
+            Camera.Picker = new PickingTexture(Scene.GuiContext, OnPicked);
+
+            CreateBuffers();
+
+            var timer = Stopwatch.StartNew();
+            PreSceneLoad();
+            LoadScene();
+            timer.Stop();
+            Log.Debug(GetType().Name, $"Loading scene time: {timer.Elapsed}, shader variants: {GuiContext.ShaderLoader.ShaderCount}, materials: {GuiContext.MaterialLoader.MaterialCount}");
+
+            PostSceneLoad();
 
             GLLoad -= OnLoad;
             GLPaint += OnPaint;

GUI/GUI.csproj
@@ -38,6 +38,7 @@
     <EmbeddedResource Include="AssetTypes\*" />
     <EmbeddedResource Include="Types\Renderer\Shaders\**" />
     <EmbeddedResource Include="Utils\inspect_agents_custom_cubemap.vtex_c" />
+    <EmbeddedResource Include="Utils\ggx_integrate_brdf_lut_schlick.vtex_c" />
   </ItemGroup>
   <ItemGroup>
     <ProjectReference Include="..\ValveResourceFormat\ValveResourceFormat.csproj">

GUI/Types/Renderer/GLModelViewer.cs
@@ -140,10 +140,14 @@ namespace GUI.Types.Renderer
             });
         }
 
+        public override void PreSceneLoad()
+        {
+            base.PreSceneLoad();
+            LoadDefaultEnviromentMap();
+        }
+
         protected override void LoadScene()
         {
-            LoadDefaultEnviromentMap();
-
             if (model != null)
             {
                 modelSceneNode = new ModelSceneNode(Scene, model);
@@ -344,14 +348,12 @@ namespace GUI.Types.Renderer
                     }
                 }
             }
-
-            Scene.CalculateEnvironmentMaps();
         }
 
         private void LoadDefaultEnviromentMap()
         {
             var assembly = Assembly.GetExecutingAssembly();
-            using var stream = assembly.GetManifestResourceStream($"GUI.Utils.inspect_agents_custom_cubemap.vtex_c");
+            using var stream = assembly.GetManifestResourceStream("GUI.Utils.inspect_agents_custom_cubemap.vtex_c");
 
             using var resource = new Resource()
             {

GUI/Types/Renderer/WorldLoader.cs
@@ -43,14 +43,6 @@ namespace GUI.Types.Renderer
 
             scene.RenderAttributes.TryAdd("LightmapGameVersionNumber", (byte)scene.LightingInfo.LightmapGameVersionNumber);
 
-            var brdfLut = guiContext.MaterialLoader.LoadTexture("textures/dev/ggx_integrate_brdf_lut_schlick.vtex");
-
-            if (brdfLut != null)
-            {
-                // TODO: add annoying force clamp for lut
-                scene.LightingInfo.Lightmaps["g_tBRDFLookup"] = brdfLut;
-            }
-
             foreach (var lumpName in world.GetEntityLumpNames())
             {
                 if (lumpName == null)
@@ -90,8 +82,6 @@ namespace GUI.Types.Renderer
                     }
                 }
             }
-
-            scene.CalculateEnvironmentMaps();
 
             LoadWorldPhysics(scene);
         }

commit 714ef970732037052132b9eda53bc634d351031e

Fix skybox scene missing brdf lut and env maps
Broken in 7c648ac6059e64058f50275b5b4ddff856b49da7

GUI/Types/Renderer/GLSceneViewer.cs
@@ -153,6 +153,12 @@ namespace GUI.Types.Renderer
         {
             Scene.CalculateEnvironmentMaps();
 
+            if (SkyboxScene != null)
+            {
+                SkyboxScene.LightingInfo.Lightmaps["g_tBRDFLookup"] = Scene.LightingInfo.Lightmaps["g_tBRDFLookup"];
+                SkyboxScene.CalculateEnvironmentMaps();
+            }
+
             if (Scene.AllNodes.Any() && this is not GLWorldViewer)
             {
                 var first = true;