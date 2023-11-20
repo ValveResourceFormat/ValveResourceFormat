f002b5996577f8482da44a9a824e36bd4375ecb6
Krotter <65136069+krotter0@users.noreply.github.com>
2023-11-20 20:31:57 +0100

    Remove unnecessary usings in flex op classes

--- ValveResourceFormat/Resource/ResourceTypes/ModelFlex/FlexOps/FlexOpAdd.cs
+++ ValveResourceFormat/Resource/ResourceTypes/ModelFlex/FlexOps/FlexOpAdd.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ValveResourceFormat.ResourceTypes.ModelFlex.FlexOps
 {
     public class FlexOpAdd : FlexOp
--- ValveResourceFormat/Resource/ResourceTypes/ModelFlex/FlexOps/FlexOpNWay.cs
+++ ValveResourceFormat/Resource/ResourceTypes/ModelFlex/FlexOps/FlexOpNWay.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ValveResourceFormat.ResourceTypes.ModelFlex.FlexOps
 {
