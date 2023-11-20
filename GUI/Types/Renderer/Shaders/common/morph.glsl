0428b43587c456a2cef07fa9de44b8609b6a4b80
Krotter <65136069+krotter0@users.noreply.github.com>
2023-11-20 20:29:12 +0100

    Check F_MORPH_SUPPORTED in morph shader

--- GUI/Types/Renderer/Shaders/common/morph.glsl
+++ GUI/Types/Renderer/Shaders/common/morph.glsl
@@ -1,17 +1,26 @@
-uniform sampler2D morphCompositeTexture;
-uniform vec2 morphCompositeTextureSize;
-uniform int morphVertexIdOffset;
+#define F_MORPH_SUPPORTED 0
 
-vec2 getMorphUV()
-{
-    int vertexId = gl_VertexID + morphVertexIdOffset;
-    return vec2(
-        1.5/2048 + mod(vertexId, morphCompositeTextureSize.x * 1.0) / 2048.0,
-        1 - (1.5/2048 + floor(vertexId / morphCompositeTextureSize.x * 1.0) / 2048.0)
-    );
-}
+#if F_MORPH_SUPPORTED == 1
+    uniform sampler2D morphCompositeTexture;
+    uniform vec2 morphCompositeTextureSize;
+    uniform int morphVertexIdOffset;
 
-vec3 getMorphOffset()
-{
-    return texture(morphCompositeTexture, getMorphUV()).xyz;
-}
+    vec2 getMorphUV()
+    {
+        int vertexId = gl_VertexID + morphVertexIdOffset;
+        return vec2(
+            1.5/2048 + mod(vertexId, morphCompositeTextureSize.x) / 2048.0,
+            1 - (1.5/2048 + floor(vertexId / morphCompositeTextureSize.x) / 2048.0)
+        );
+    }
+
+    vec3 getMorphOffset()
+    {
+        return texture(morphCompositeTexture, getMorphUV()).xyz;
+    }
+#else
+    vec3 getMorphOffset()
+    {
+        return vec3(0, 0, 0);
+    }
+#endif
