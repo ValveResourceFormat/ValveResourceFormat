a9da4f82ef30d1333a438f4386880d5f5cb21d5e
kristiker <kristik06@outlook.com>
2024-02-16 21:28:22 +0100

    Parse old SoundEventScript resource into far more useful KVObject

--- ValveResourceFormat/Resource/ResourceTypes/SoundEventScript.cs
+++ ValveResourceFormat/Resource/ResourceTypes/SoundEventScript.cs
@@ -1,51 +1,29 @@
 using System.IO;
+using System.Text;
+using ValveResourceFormat.Serialization.KeyValues;
 
 namespace ValveResourceFormat.ResourceTypes
 {
     public class SoundEventScript : NTRO
     {
-        public Dictionary<string, string> SoundEventScriptValue { get; private set; } // TODO: be Dictionary<string, SomeKVObject>
+        public Dictionary<string, KVObject> SoundEventScriptValue { get; private set; }
 
         public override void Read(BinaryReader reader, Resource resource)
         {
             base.Read(reader, resource);
-            SoundEventScriptValue = [];
 
-            /* TODO
-            // Output is VSoundEventScript_t we need to iterate m_SoundEvents inside it.
-            var soundEvents = (NTROArray)Output["m_SoundEvents"];
+            var soundEvents = Output.GetArray<KVObject>("m_SoundEvents");
+            SoundEventScriptValue = new(capacity: soundEvents.Length);
 
             foreach (var entry in soundEvents)
             {
-                // sound is VSoundEvent_t
-                var sound = ((NTROValue<NTROStruct>)entry).Value;
-                var soundName = ((NTROValue<string>)sound["m_SoundName"]).Value;
-                var soundValue = ((NTROValue<string>)sound["m_OperatorsKV"]).Value.Replace("\n", Environment.NewLine, StringComparison.InvariantCulture); // make sure we have new lines
+                var soundName = entry.GetProperty<string>("m_SoundName");
+                var soundValue = entry.GetProperty<string>("m_OperatorsKV");
+                using var ms = new MemoryStream(Encoding.UTF8.GetBytes(soundValue));
 
                 // Valve have duplicates, assume last is correct?
-                SoundEventScriptValue.Remove(soundName);
-
-                SoundEventScriptValue.Add(soundName, soundValue);
+                SoundEventScriptValue[soundName] = KeyValues3.ParseKVFile(ms).Root;
             }
-            */
-        }
-
-        public override string ToString()
-        {
-            using var writer = new IndentedTextWriter();
-            foreach (var entry in SoundEventScriptValue)
-            {
-                writer.WriteLine("\"" + entry.Key + "\"");
-                writer.WriteLine("{");
-                writer.Indent++;
-                // m_OperatorsKV wont be indented, so we manually indent it here, removing the last indent so we can close brackets later correctly.
-                writer.Write(entry.Value.Replace(Environment.NewLine, Environment.NewLine + "\t", StringComparison.InvariantCulture).TrimEnd('\t'));
-                writer.Indent--;
-                writer.WriteLine("}");
-                writer.WriteLine(string.Empty); // There is an empty line after every entry (including the last)
-            }
-
-            return writer.ToString();
         }
     }
 }
