ValveResourceFormat/ValveResourceFormat

commit 866ade0874e41b5460c9d9ae98269febb962794e

Only set opacity for F_TRANSLUCENT

GUI/Types/Renderer/Shaders/complex.frag
@@ -366,7 +366,10 @@ MaterialProperties_t GetMaterial(vec2 texCoord, vec3 vertexNormals)
 #endif
 
     mat.Albedo = color.rgb;
+
+#if (F_TRANSLUCENT == 1)
     mat.Opacity = color.a;
+#endif
 
 #if defined(static_overlay_vfx_common) && (F_PAINT_VERTEX_COLORS == 1)
     mat.Albedo *= vVertexColorOut.rgb;

commit 8c3ac17f28324874b63a86db8a0cc1b80f0225c2

Fix overlays not being transparent on shader side
Broken in 866ade0874e41b5460c9d9ae98269febb962794e

GUI/Types/Renderer/Shaders/complex.frag
@@ -162,7 +162,7 @@ uniform sampler2D g_tTintMask;
 
 #define unlit (defined(vr_unlit_vfx) || defined(unlit_vfx) || defined(csgo_unlitgeneric_vfx) || (F_FULLBRIGHT == 1) || (F_UNLIT == 1) || (defined(static_overlay_vfx_common) && F_LIT == 0))
 #define alphatest (F_ALPHA_TEST == 1) || ((defined(csgo_unlitgeneric_vfx) || defined(static_overlay_vfx_common)) && (F_BLEND_MODE == 2))
-#define translucent (F_TRANSLUCENT == 1) || (F_GLASS == 1) || defined(glass_vfx_common) || ((defined(csgo_unlitgeneric_vfx) || defined(static_overlay_vfx_common)) && (F_BLEND_MODE == 1)) // need to set this up on the cpu side
+#define translucent (F_TRANSLUCENT == 1) || (F_GLASS == 1) || (F_BLEND_MODE > 0 && F_BLEND_MODE != 2) || defined(glass_vfx_common) || ((defined(csgo_unlitgeneric_vfx) || defined(static_overlay_vfx_common)) && (F_BLEND_MODE == 1)) // need to set this up on the cpu side
 #define blendMod2x (F_BLEND_MODE == 3)
 
 #if (alphatest == 1)