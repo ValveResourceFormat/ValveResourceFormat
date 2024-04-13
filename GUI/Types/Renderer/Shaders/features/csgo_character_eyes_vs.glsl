c1773ce5b7c03bcd182322af90a401d16e16e315
kristiker <kristik06@outlook.com>
2024-04-13 18:13:32 +0200

    Extract more duplicate code

--- GUI/Types/Renderer/Shaders/features/csgo_character_eyes_vs.glsl
+++ GUI/Types/Renderer/Shaders/features/csgo_character_eyes_vs.glsl
@@ -32,6 +32,24 @@ uniform int g_nEyeTargetBindIdx;
 
 out EyePixelInput eyeInterpolator;
 
+mat3 rotationMatrix(vec3 axis, float angle)
+{
+    // original
+    // return mat3(
+    //     vec3(fma(axis.x, axis.x, fma(-axis.x, axis.x, 1.0) * c), fma(axis.x * axis.y, oc, axis.z * (-s)), fma(axis.z * axis.x, oc, axis.y * s)),
+    //     vec3(fma(axis.x * axis.y, oc, axis.z * s), fma(axis.y, axis.y, fma(-axis.y, axis.y, 1.0) * c), fma(axis.y * axis.z, oc, axis.x * (-s))),
+    //     vec3(fma(axis.z * axis.x, oc, axis.y * (-s)), fma(axis.y * axis.z, oc, axis.x * s), fma(axis.z, axis.z, fma(-axis.z, axis.z, 1.0) * c))
+    // );
+
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+    return mat3(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c);
+}
+
 EyePixelInput GetCharacterEyeInterpolator(vec3 vPositionOs)
 {
     EyeData leftEyeOs;
@@ -54,93 +72,48 @@ EyePixelInput GetCharacterEyeInterpolator(vec3 vPositionOs)
     mat4 eyeAnimationMatrix = getMatrix(currentEye.BoneIndex);
     mat3 eyeRotationMatrix = mat3(eyeAnimationMatrix);
 
-    mat4 resultMatrix = mat4(1.0);
-    resultMatrix[0] = vec4(eyeRotationMatrix * currentEye.Forward, 0.0);
-    resultMatrix[2] = vec4(eyeRotationMatrix * currentEye.Up, 0.0);
-    resultMatrix[3] = eyeAnimationMatrix * vec4(currentEye.Position, 1.0);
-
-    // todo: animatedEye .Position .Rotation .Transform
+    currentEye.Forward =    eyeRotationMatrix * currentEye.Forward;
+    currentEye.Up =         eyeRotationMatrix * currentEye.Up;
+    currentEye.Position =   (eyeAnimationMatrix * vec4(currentEye.Position, 1.0)).xyz;
 
     vec3 targetPosition = (getMatrix(g_nEyeTargetBindIdx) * vec4(g_vEyeTargetBindPos, 1.0)).xyz;
-    vec3 viewDir = normalize(targetPosition - resultMatrix[3].xyz);
+    vec3 viewDir = normalize(targetPosition - currentEye.Position);
 
     if (flExotropia != 0.0)
     {
         flExotropia = radians(flExotropia);
-        float sinRot = sin(flExotropia);
-        float cosRot = cos(flExotropia);
 
-        float _15300 = resultMatrix[2].x * resultMatrix[2].y;
-        float _12989 = 1.0 - cosRot;
-        float _10616 = resultMatrix[2].z * sinRot;
-        float _14840 = resultMatrix[2].z * resultMatrix[2].x;
-        float _16712 = resultMatrix[2].y * sinRot;
-        float _12705 = resultMatrix[2].y * resultMatrix[2].z;
-        float _16077 = resultMatrix[2].x * sinRot;
-
-        mat3 wallEyeInfluence = mat3(
-            vec3(fma(resultMatrix[2].x, resultMatrix[2].x, fma(-resultMatrix[2].x, resultMatrix[2].x, 1.0) * cosRot), fma(_15300, _12989, -_10616), fma(_14840, _12989, _16712)),
-            vec3(fma(_15300, _12989, _10616), fma(resultMatrix[2].y, resultMatrix[2].y, fma(-resultMatrix[2].y, resultMatrix[2].y, 1.0) * cosRot), fma(_12705, _12989, -_16077)),
-            vec3(fma(_14840, _12989, -_16712), fma(_12705, _12989, _16077), fma(resultMatrix[2].z, resultMatrix[2].z, fma(-resultMatrix[2].z, resultMatrix[2].z, 1.0) * cosRot))
-        );
-
-        targetPosition = ((viewDir * wallEyeInfluence) * distance(targetPosition, resultMatrix[3].xyz)) + resultMatrix[3].xyz;
-        viewDir = normalize(targetPosition - resultMatrix[3].xyz);
+        mat3 wallEyeInfluence = rotationMatrix(currentEye.Up, flExotropia);
+        targetPosition = ((viewDir * wallEyeInfluence) * distance(targetPosition, currentEye.Position)) + currentEye.Position;
+        viewDir = normalize(targetPosition - currentEye.Position);
     }
 
     const float radians40 = radians(40.0);
-    const float sin40 = sin(radians40);
-    const float cos40 = cos(radians40);
-    const float oneMinusCos40 = 1.0 - cos40;
 
-    vec3 testVector = normalize(resultMatrix[0].xyz + (resultMatrix[2].xyz * 0.5));
+    vec3 testVector = normalize(currentEye.Forward + (currentEye.Up * 0.5));
     if (acos(dot(viewDir, testVector)) > radians40)
     {
-        vec3 _17148 = normalize(cross(testVector, viewDir));
-        float _25223 = _17148.x;
-        float _6863 = _17148.y;
-        float _13016 = _17148.z;
-        float _12706 = _25223 * _6863;
-        float _14954 = _13016 * _25223;
-        float _12707 = _6863 * _13016;
+        vec3 axis = normalize(cross(testVector, viewDir));
+        mat3 rotation = rotationMatrix(axis, radians40);
 
-        // todo: extract to helper method
-        mat3 rotation = mat3(
-            vec3(fma(_25223, _25223, fma(-_25223, _25223, 1.0) * cos40), fma(_12706, oneMinusCos40, _13016 * (-sin40)), fma(_14954, oneMinusCos40, _6863 * sin40)),
-            vec3(fma(_12706, oneMinusCos40, _13016 * sin40), fma(_6863, _6863, fma(-_6863, _6863, 1.0) * cos40), fma(_12707, oneMinusCos40, _25223 * (-sin40))),
-            vec3(fma(_14954, oneMinusCos40, _6863 * (-sin40)), fma(_12707, oneMinusCos40, _25223 * sin40), fma(_13016, _13016, fma(-_13016, _13016, 1.0) * cos40))
-        );
-
-        targetPosition = ((testVector * rotation) * distance(targetPosition, resultMatrix[3].xyz)) + resultMatrix[3].xyz;
-        viewDir = normalize(targetPosition - resultMatrix[3].xyz);
+        targetPosition = ((testVector * rotation) * distance(targetPosition, currentEye.Position)) + currentEye.Position;
+        viewDir = normalize(targetPosition - currentEye.Position);
     }
 
-    vec3 testVector2 = normalize(resultMatrix[0].xyz + (resultMatrix[2].xyz * (-0.5)));
+    vec3 testVector2 = normalize(currentEye.Forward + (currentEye.Up * (-0.5)));
     if (acos(dot(viewDir, testVector2)) > radians40)
     {
-        vec3 _17148 = normalize(cross(testVector2, viewDir));
-        float _25223 = _17148.x;
-        float _6863 = _17148.y;
-        float _13016 = _17148.z;
-        float _12706 = _25223 * _6863;
-        float _14954 = _13016 * _25223;
-        float _12707 = _6863 * _13016;
+        vec3 axis = normalize(cross(testVector2, viewDir));
+        mat3 rotation = rotationMatrix(axis, radians40);
 
-        // todo: extract to helper method
-        mat3 rotation = mat3(
-            vec3(fma(_25223, _25223, fma(-_25223, _25223, 1.0) * cos40), fma(_12706, oneMinusCos40, _13016 * (-sin40)), fma(_14954, oneMinusCos40, _6863 * sin40)),
-            vec3(fma(_12706, oneMinusCos40, _13016 * sin40), fma(_6863, _6863, fma(-_6863, _6863, 1.0) * cos40), fma(_12707, oneMinusCos40, _25223 * (-sin40))),
-            vec3(fma(_14954, oneMinusCos40, _6863 * (-sin40)), fma(_12707, oneMinusCos40, _25223 * sin40), fma(_13016, _13016, fma(-_13016, _13016, 1.0) * cos40))
-        );
-
-        vec3 targetPosition2 = ((testVector2 * rotation) * distance(targetPosition, resultMatrix[3].xyz)) + resultMatrix[3].xyz;
-        viewDir = normalize(targetPosition2 - resultMatrix[3].xyz);
+        vec3 targetPosition2 = ((testVector2 * rotation) * distance(targetPosition, currentEye.Position)) + currentEye.Position;
+        viewDir = normalize(targetPosition2 - currentEye.Position);
     }
 
     EyePixelInput o;
-    o.vEyeViewDir_PosX =  vec4(viewDir,                                 resultMatrix[3].x);
-    o.vEyeRightDir_PosY = vec4(cross(resultMatrix[2].xyz, viewDir),     resultMatrix[3].y);
-    o.vEyeUpDir_PosZ =    vec4(cross(viewDir, o.vEyeRightDir_PosY.xyz), resultMatrix[3].z);
+    o.vEyeViewDir_PosX =  vec4(viewDir,                                 currentEye.Position.x);
+    o.vEyeRightDir_PosY = vec4(cross(currentEye.Up, viewDir),           currentEye.Position.y);
+    o.vEyeUpDir_PosZ =    vec4(cross(viewDir, o.vEyeRightDir_PosY.xyz), currentEye.Position.z);
     return o;
 }
 #endif // defined(csgo_character_vfx) && (F_EYEBALLS == 1)
