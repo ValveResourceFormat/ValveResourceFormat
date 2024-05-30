94160fb26973ba5290a1ceeb4adc5dce85198d49
kristiker <kristik06@outlook.com>
2024-05-30 12:08:45 +0200

    Add new VfxEval functions

--- ValveResourceFormat/Serialization/VfxEval.cs
+++ ValveResourceFormat/Serialization/VfxEval.cs
@@ -72,6 +72,8 @@ namespace ValveResourceFormat.Serialization.VfxEval
             ("degrees",             1), // 36
             ("MatrixColorTint2",    2), // 37
             ("MatrixColorTint3",    3), // 38
+            ("RemapVal",            4), // 39
+            ("RemapValClamped",     4), // 3A
 #pragma warning restore format
         ];
 
