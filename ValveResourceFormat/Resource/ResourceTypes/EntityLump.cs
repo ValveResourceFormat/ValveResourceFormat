d1673cd419ba9e600cbfb44aa7482363f419c17c
田七不甜 <1633557643@qq.com>
2024-06-27 05:13:56 +0800

    Fix vents_c `timesToFire` is not `1` & `-1` error

--- ValveResourceFormat/Resource/ResourceTypes/EntityLump.cs
+++ ValveResourceFormat/Resource/ResourceTypes/EntityLump.cs
@@ -305,13 +305,17 @@ namespace ValveResourceFormat.ResourceTypes
 
                         var timesToFire = connection.GetInt32Property("m_nTimesToFire");
 
-                        if (timesToFire == 1)
+                        switch (timesToFire)
                         {
-                            builder.Append("OnlyOnce ");
-                        }
-                        else if (timesToFire != -1)
-                        {
-                            throw new UnexpectedMagicException("Unexpected times to fire", timesToFire, nameof(timesToFire));
+                            case 1:
+                                builder.Append("OnlyOnce ");
+                                break;
+                            case 2:
+                                builder.Append("OnlyTwice ");
+                                break;
+                            case >= 3:
+                                builder.Append($"Only{timesToFire}Times ");
+                                break;
                         }
 
                         builder.Append(connection.GetProperty<string>("m_inputName"));
