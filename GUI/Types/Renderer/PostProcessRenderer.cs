009eead98659b7f731f8ee47a594f15ea2893985
kristiker <kristik06@outlook.com>
2024-12-04 11:05:51 +0100

    Unbind program and vao, so we swap buffers on a clean state

--- GUI/Types/Renderer/PostProcessRenderer.cs
+++ GUI/Types/Renderer/PostProcessRenderer.cs
@@ -74,6 +74,9 @@ namespace GUI.Types.Renderer
             GL.BindVertexArray(vao);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
+            GL.UseProgram(0);
+            GL.BindVertexArray(0);
+
             GL.DepthMask(true);
             GL.Enable(EnableCap.DepthTest);
         }
