f9066efde8ee7c98ce7497e8faeb67e9128ef7ea
Krotter <65136069+krotter0@users.noreply.github.com>
2024-12-15 16:59:02 +0100

    Replace test files with smaller ones

--- Tests/NavTests.cs
+++ Tests/NavTests.cs
@@ -32,12 +32,12 @@ namespace Tests
         [Test]
         public void TestNavVersion30_NavGenVersion7()
         {
-            var navMeshFile = GetNavMesh("a2_drainage.nav");
+            var navMeshFile = GetNavMesh("a5_vault.nav");
             Assert.Multiple(() =>
             {
                 Assert.That(navMeshFile.Version, Is.EqualTo(30));
                 Assert.That(navMeshFile.SubVersion, Is.EqualTo(0));
-                Assert.That(navMeshFile.Areas.Count, Is.EqualTo(7258));
+                Assert.That(navMeshFile.Areas.Count, Is.EqualTo(1245));
                 Assert.That(navMeshFile.GenerationParams.NavGenVersion, Is.EqualTo(7));
                 Assert.That(navMeshFile.GenerationParams.HullParams[2].MaxJumpUpDist, Is.EqualTo(240));
             });
@@ -46,13 +46,13 @@ namespace Tests
         [Test]
         public void TestNavVersion35()
         {
-            var navMeshFile = GetNavMesh("de_mirage.nav");
+            var navMeshFile = GetNavMesh("lobby_mapveto.nav");
             Assert.Multiple(() =>
             {
                 Assert.That(navMeshFile.Version, Is.EqualTo(35));
                 Assert.That(navMeshFile.SubVersion, Is.EqualTo(1));
-                Assert.That(navMeshFile.Areas.Count, Is.EqualTo(2519));
-                Assert.That(navMeshFile.Ladders.Count, Is.EqualTo(2));
+                Assert.That(navMeshFile.Areas.Count, Is.EqualTo(4));
+                Assert.That(navMeshFile.Ladders.Count, Is.EqualTo(0));
                 Assert.That(navMeshFile.GenerationParams.NavGenVersion, Is.EqualTo(12));
                 Assert.That(navMeshFile.CustomData, Is.Not.Null);
             });
