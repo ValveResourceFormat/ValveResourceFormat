a78b6cd1d8942c50c5bd70db505652c34a384dda
kristiker <kristik06@outlook.com>
2024-12-22 18:42:23 +0100

    Correct NavMesh animation disable

--- GUI/Types/Renderer/NavMeshSceneNode.cs
+++ GUI/Types/Renderer/NavMeshSceneNode.cs
@@ -133,7 +133,7 @@ namespace GUI.Types.Renderer
             GL.UseProgram(renderShader.Program);
 
             renderShader.SetUniform4x4("transform", Transform);
-            renderShader.SetUniform1("bAnimated", 0.0f);
+            renderShader.SetBoneAnimationData(false);
             renderShader.SetUniform1("sceneObjectId", Id);
 
             renderShader.SetUniform1("g_bNormalShaded", true);
