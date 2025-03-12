ValveResourceFormat/ValveResourceFormat

commit 426f5c60341fe12b13931430e6eea54b947a46d9

Always keep a `Resource` reference in all blocks, remove resource argument on `Block.Read` (#887)
* Change sentenceOffset from long to uint

* All resource blocks now have a Resource reference, remove resource argument from Read

* Make editinfo a property

* Keep resource reference in NTRO

* Fix decompiler build

* Remove nesting

* Handle ntro and redi separately

* Add obsolete method incase it breaks user code

ValveResourceFormat/Resource/Resource.cs
@@ -232,59 +232,56 @@ namespace ValveResourceFormat
 
                 block.Offset = offset;
                 block.Size = size;
+                block.Resource = this;
 
                 Blocks.Add(block);
 
-                switch (block.Type)
+                if (block.Type is BlockType.NTRO)
                 {
-                    case BlockType.REDI:
-                    case BlockType.RED2:
-                        block.Read(Reader, this);
+                    block.Read(Reader);
+                }
 
-                        EditInfo = (ResourceEditInfo)block;
-
-                        // Try to determine resource type by looking at the compiler indentifiers
-                        if (ResourceType == ResourceType.Unknown)
-                        {
-                            foreach (var specialDep in EditInfo.SpecialDependencies)
-                            {
-                                ResourceType = DetermineResourceTypeByCompilerIdentifier(specialDep);
-
-                                if (ResourceType != ResourceType.Unknown)
-                                {
-                                    break;
-                                }
-                            }
-                        }
-
-                        // Try to determine resource type by looking at the input dependency if there is only one
-                        if (ResourceType == ResourceType.Unknown && EditInfo.InputDependencies.Count == 1)
-                        {
-                            ResourceType = ResourceTypeExtensions.DetermineByFileExtension(Path.GetExtension(EditInfo.InputDependencies[0].ContentRelativeFilename));
-                        }
-
-                        break;
-
-                    case BlockType.NTRO:
-                        block.Read(Reader, this);
-                        break;
+                if (block.Type is BlockType.RED2 or BlockType.REDI)
+                {
+                    block.Read(Reader);
+                    EditInfo = (ResourceEditInfo)block;
                 }
 
                 Reader.BaseStream.Position = position + 8;
+            }
+
+            // Try to determine resource type by looking at the compiler indentifiers
+            if (ResourceType == ResourceType.Unknown && EditInfo != null)
+            {
+                foreach (var specialDep in EditInfo.SpecialDependencies)
+                {
+                    ResourceType = DetermineResourceTypeByCompilerIdentifier(specialDep);
+
+                    if (ResourceType != ResourceType.Unknown)
+                    {
+                        break;
+                    }
+                }
+
+                // Try to determine resource type by looking at the input dependency if there is only one
+                if (ResourceType == ResourceType.Unknown && EditInfo.InputDependencies.Count == 1)
+                {
+                    ResourceType = ResourceTypeExtensions.DetermineByFileExtension(Path.GetExtension(EditInfo.InputDependencies[0].ContentRelativeFilename));
+                }
             }
 
             foreach (var block in Blocks)
             {
                 if (block.Type is not BlockType.REDI and not BlockType.RED2 and not BlockType.NTRO)
                 {
-                    block.Read(Reader, this);
+                    block.Read(Reader);
                 }
             }
 
             if (ResourceType == ResourceType.Sound && ContainsBlockType(BlockType.CTRL)) // Version >= 5, but other ctrl-type sounds have version 0
             {
                 var block = new Sound();
-                block.ConstructFromCtrl(Reader, this);
+                block.ConstructFromCtrl(Reader);
                 Blocks.Add(block);
             }
 

Decompiler/Decompiler.cs
@@ -781,7 +781,7 @@ namespace Decompiler
                 using (var binaryReader = new BinaryReader(stream))
                 {
                     kv3.Size = (uint)stream.Length;
-                    kv3.Read(binaryReader, null);
+                    kv3.Read(binaryReader);
                 }
 
                 Console.WriteLine(kv3.ToString());

GUI/Types/Viewers/BinaryKeyValues.cs
@@ -28,7 +28,7 @@ namespace GUI.Types.Viewers
             using (var binaryReader = new BinaryReader(kv3stream))
             {
                 kv3.Size = (uint)kv3stream.Length;
-                kv3.Read(binaryReader, null);
+                kv3.Read(binaryReader);
             }
 
             kv3stream.Close();

ValveResourceFormat/NavMesh/NavMeshFile.cs
@@ -103,7 +103,7 @@ namespace ValveResourceFormat.NavMesh
                 var kv3 = new BinaryKV3();
                 kv3.Offset = (uint)(binaryReader.BaseStream.Position - 1);
                 kv3.Size = (uint)(binaryReader.BaseStream.Length - kv3.Offset);
-                kv3.Read(binaryReader, null);
+                kv3.Read(binaryReader);
 
                 CustomData = kv3.Data;
             }

ValveResourceFormat/Resource/Block.cs
@@ -22,7 +22,19 @@ namespace ValveResourceFormat
         /// </summary>
         public uint Size { get; set; }
 
-        public abstract void Read(BinaryReader reader, Resource resource);
+        /// <summary>
+        /// Gets the resource this block belongs to.
+        /// </summary>
+        public Resource Resource { get; set; }
+
+        public abstract void Read(BinaryReader reader);
+
+        [Obsolete("Use Read(BinaryReader) and the Resource property.")]
+        public void Read(BinaryReader reader, Resource resource)
+        {
+            Resource = resource;
+            Read(reader);
+        }
 
         /// <summary>
         /// Returns a string that represents the current object.

ValveResourceFormat/Resource/Blocks/ResourceData.cs
@@ -9,7 +9,7 @@ namespace ValveResourceFormat.Blocks
     {
         public override BlockType Type => BlockType.DATA;
 
-        public override void Read(BinaryReader reader, Resource resource)
+        public override void Read(BinaryReader reader)
         {
             // TODO
         }

ValveResourceFormat/Resource/Blocks/ResourceEditInfo.cs
@@ -20,7 +20,7 @@ namespace ValveResourceFormat.Blocks
         public List<string> ChildResourceList { get; } = [];
         public KVObject SearchableUserData { get; } = new("m_SearchableUserData"); // Maybe these should be split..
 
-        public override void Read(BinaryReader reader, Resource resource)
+        public override void Read(BinaryReader reader)
         {
             var subBlock = 0;
 

ValveResourceFormat/Resource/Blocks/ResourceEditInfo2.cs
@@ -24,15 +24,16 @@ namespace ValveResourceFormat.Blocks
             //
         }
 
-        public override void Read(BinaryReader reader, Resource resource)
+        public override void Read(BinaryReader reader)
         {
             var kv3 = new BinaryKV3
             {
                 Offset = Offset,
                 Size = Size,
+                Resource = Resource,
             };
 
-            kv3.Read(reader, resource);
+            kv3.Read(reader);
             BackingData = kv3;
 
             static void ReadItems<T>(BinaryKV3 kv3, List<T> list, string key, Func<KVObject, T> constructor)

commit c34c126acaba075c91a5ab0322aabdaa29e8d698

Determine resource type right after reading REDI
Fixes #903
Broken in 426f5c60341fe12b13931430e6eea54b947a46d9

ValveResourceFormat/Resource/Resource.cs
@@ -243,29 +243,31 @@ namespace ValveResourceFormat
                 {
                     block.Read(Reader);
                     EditInfo = (ResourceEditInfo)block;
-                }
 
-                Reader.BaseStream.Position = position + 8;
-            }
-
-            // Try to determine resource type by looking at the compiler indentifiers
-            if (ResourceType == ResourceType.Unknown && EditInfo != null)
-            {
-                foreach (var specialDep in EditInfo.SpecialDependencies)
-                {
-                    ResourceType = DetermineResourceTypeByCompilerIdentifier(specialDep);
-
-                    if (ResourceType != ResourceType.Unknown)
+                    // Try to determine resource type by looking at the compiler indentifiers
+                    // This must be done right after reading EditInfo because future DATA block
+                    // will depend on knowing the resource type to construct the correct block in ConstructResourceType()
+                    if (ResourceType == ResourceType.Unknown && EditInfo != null)
                     {
-                        break;
+                        foreach (var specialDep in EditInfo.SpecialDependencies)
+                        {
+                            ResourceType = DetermineResourceTypeByCompilerIdentifier(specialDep);
+
+                            if (ResourceType != ResourceType.Unknown)
+                            {
+                                break;
+                            }
+                        }
+
+                        // Try to determine resource type by looking at the input dependency if there is only one
+                        if (ResourceType == ResourceType.Unknown && EditInfo.InputDependencies.Count == 1)
+                        {
+                            ResourceType = ResourceTypeExtensions.DetermineByFileExtension(Path.GetExtension(EditInfo.InputDependencies[0].ContentRelativeFilename));
+                        }
                     }
                 }
 
-                // Try to determine resource type by looking at the input dependency if there is only one
-                if (ResourceType == ResourceType.Unknown && EditInfo.InputDependencies.Count == 1)
-                {
-                    ResourceType = ResourceTypeExtensions.DetermineByFileExtension(Path.GetExtension(EditInfo.InputDependencies[0].ContentRelativeFilename));
-                }
+                Reader.BaseStream.Position = position + 8;
             }
 
             foreach (var block in Blocks)

Tests/SoundTest.cs
@@ -13,13 +13,47 @@ namespace Tests
         public void TestSound()
         {
             var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "beep.vsnd_c");
-            using var resource = new Resource
-            {
-                FileName = file,
-            };
+            using var resource = new Resource();
             resource.Read(file);
 
             Assert.That(resource.ResourceType, Is.EqualTo(ResourceType.Sound));
+            Assert.That(resource.DataBlock, Is.InstanceOf<Sound>());
+
+            using var hash = SHA256.Create();
+            using var sound = ((Sound)resource.DataBlock).GetSoundStream();
+            var actualHash = Convert.ToHexString(hash.ComputeHash(sound));
+
+            Assert.That(actualHash, Is.EqualTo("1F8BF83F3E827A3C02C6AE6B6BD23BBEBD4E18C4F877D092CF0C5B800DAAB2B7"));
+        }
+
+        [Test]
+        public void TestSoundNoFileName()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "beep.vsnd_c");
+            using var fs = File.OpenRead(file);
+            using var resource = new Resource();
+            resource.Read(fs, verifyFileSize: false);
+
+            Assert.That(resource.ResourceType, Is.EqualTo(ResourceType.Sound));
+            Assert.That(resource.DataBlock, Is.InstanceOf<Sound>());
+
+            using var hash = SHA256.Create();
+            using var sound = ((Sound)resource.DataBlock).GetSoundStream();
+            var actualHash = Convert.ToHexString(hash.ComputeHash(sound));
+
+            Assert.That(actualHash, Is.EqualTo("1F8BF83F3E827A3C02C6AE6B6BD23BBEBD4E18C4F877D092CF0C5B800DAAB2B7"));
+        }
+
+        [Test]
+        public void TestSoundNoFileNameVerifySize()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "beep.vsnd_c");
+            using var fs = File.OpenRead(file);
+            using var resource = new Resource();
+            resource.Read(fs);
+
+            Assert.That(resource.ResourceType, Is.EqualTo(ResourceType.Sound));
+            Assert.That(resource.DataBlock, Is.InstanceOf<Sound>());
 
             using var hash = SHA256.Create();
             using var sound = ((Sound)resource.DataBlock).GetSoundStream();

Tests/Test.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 using ValveResourceFormat;
+using ValveResourceFormat.Blocks;
 using ValveResourceFormat.IO;
 using ValveResourceFormat.Utils;
 
@@ -95,6 +96,30 @@ namespace Tests
                 ms.Seek(0, SeekOrigin.Begin);
 
                 resource.Read(ms);
+
+                Assert.That(resource.DataBlock, Is.Not.TypeOf<ResourceData>());
+            }
+        }
+
+        [Test]
+        public void ReadBlocksNoFileName()
+        {
+            var resources = new Dictionary<string, Resource>();
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files");
+            var files = Directory.GetFiles(path, "*.*_c");
+
+            if (files.Length == 0)
+            {
+                Assert.Fail("There are no files to test.");
+            }
+
+            foreach (var file in files)
+            {
+                using var resource = new Resource();
+                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+                resource.Read(fs);
+
+                Assert.That(resource.DataBlock, Is.Not.TypeOf<ResourceData>());
             }
         }
 