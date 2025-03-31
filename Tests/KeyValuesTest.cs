a870ef4f086bbdbb55e2eb808fc4e1ba4995affe
Kashimura <kashimura@qq.com>
2025-03-31 22:27:09 +0800

    Add binary blob text value parse

--- Tests/KeyValuesTest.cs
+++ Tests/KeyValuesTest.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using NUnit.Framework;
 using ValveResourceFormat.Serialization.KeyValues;
 
@@ -35,7 +36,7 @@ namespace Tests
                 Assert.That(file.Encoding, Is.EqualTo("text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d}"));
                 Assert.That(file.Format, Is.EqualTo("generic:version{7412167c-06e9-4698-aff2-e63eb59037e7}"));
 
-                Assert.That(file.Root, Has.Count.EqualTo(13));
+                Assert.That(file.Root, Has.Count.EqualTo(14));
 
                 var properties = file.Root.Properties;
 
@@ -69,6 +70,10 @@ namespace Tests
                 var objectValue = properties["objectValue"].Value as KVObject;
                 Assert.That(objectValue.Properties["n"].Value, Is.EqualTo((long)5));
                 Assert.That(objectValue.Properties["s"].Value, Is.EqualTo("foo"));
+
+                var binaryBlobValue = properties["binaryBlobValue"].Value as byte[];
+                Assert.That(binaryBlobValue, Has.Length.EqualTo(40));
+                Assert.That(Encoding.UTF8.GetString(binaryBlobValue), Is.EqualTo("Hello, this is a test binary blob value!"));
 
                 Assert.That(properties["arrayOnSingleLine"].Type, Is.EqualTo(KVType.ARRAY));
 
