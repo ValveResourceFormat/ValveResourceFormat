bed6e64425b078ae9d5fa9517e8ca51db585375f
Pavel Djundik <xPaw@users.noreply.github.com>
2025-04-04 13:40:22 +0300

    Check against hdr const

--- ValveResourceFormat/TextureDecoders/DecodeR16F.cs
+++ ValveResourceFormat/TextureDecoders/DecodeR16F.cs
@@ -10,7 +10,7 @@ namespace ValveResourceFormat.TextureDecoders
             using var pixels = bitmap.PeekPixels();
             var inputPixels = MemoryMarshal.Cast<byte, Half>(input);
 
-            if (bitmap.ColorType == SKColorType.RgbaF32)
+            if (bitmap.ColorType == ResourceTypes.Texture.HdrBitmapColorType)
             {
                 DecodeHdr(pixels, inputPixels);
                 return;
