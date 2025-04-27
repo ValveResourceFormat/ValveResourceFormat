2af18fa1ab90bcee4ab953aea82e5bf6fabf96f4
Pavel Djundik <xPaw@users.noreply.github.com>
2025-04-27 11:05:51 +0300

    Use arraypool in QuadIndexBuffer

--- GUI/Types/Renderer/QuadIndexBuffer.cs
+++ GUI/Types/Renderer/QuadIndexBuffer.cs
@@ -1,3 +1,5 @@
+using System.Buffers;
+using System.Runtime.InteropServices;
 using OpenTK.Graphics.OpenGL;
 
 namespace GUI.Types.Renderer
@@ -14,20 +16,32 @@ namespace GUI.Types.Renderer
 #if DEBUG
             var bufferLabel = nameof(QuadIndexBuffer);
             GL.ObjectLabel(ObjectLabelIdentifier.Buffer, handle, bufferLabel.Length, bufferLabel);
+
+            System.Diagnostics.Debug.Assert(size % 6 == 0);
 #endif
 
-            var indices = new ushort[size];
-            for (var i = 0; i < size / 6; ++i)
-            {
-                indices[(i * 6) + 0] = (ushort)((i * 4) + 0);
-                indices[(i * 6) + 1] = (ushort)((i * 4) + 1);
-                indices[(i * 6) + 2] = (ushort)((i * 4) + 2);
-                indices[(i * 6) + 3] = (ushort)((i * 4) + 0);
-                indices[(i * 6) + 4] = (ushort)((i * 4) + 2);
-                indices[(i * 6) + 5] = (ushort)((i * 4) + 3);
-            }
+            var sizeInBytes = size * sizeof(ushort);
+            var indicesBytes = ArrayPool<byte>.Shared.Rent(sizeInBytes);
 
-            GL.NamedBufferData(handle, size * sizeof(ushort), indices, BufferUsageHint.StaticDraw);
+            try
+            {
+                var indices = MemoryMarshal.Cast<byte, ushort>(indicesBytes);
+                for (var i = 0; i < size / 6; ++i)
+                {
+                    indices[(i * 6) + 0] = (ushort)((i * 4) + 0);
+                    indices[(i * 6) + 1] = (ushort)((i * 4) + 1);
+                    indices[(i * 6) + 2] = (ushort)((i * 4) + 2);
+                    indices[(i * 6) + 3] = (ushort)((i * 4) + 0);
+                    indices[(i * 6) + 4] = (ushort)((i * 4) + 2);
+                    indices[(i * 6) + 5] = (ushort)((i * 4) + 3);
+                }
+
+                GL.NamedBufferData(handle, sizeInBytes, indicesBytes, BufferUsageHint.StaticDraw);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(indicesBytes);
+            }
         }
     }
 }
