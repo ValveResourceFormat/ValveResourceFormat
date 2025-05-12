1116f872766a183a0d4988ad14ef63684ae9abee
Pavel Djundik <xPaw@users.noreply.github.com>
2025-05-12 23:10:06 +0300

    Make shaders/choreo panels resizable

--- GUI/Controls/TextControl.Designer.cs
+++ GUI/Controls/TextControl.Designer.cs
@@ -31,46 +31,35 @@ namespace GUI.Controls
         /// </summary>
         private void InitializeComponent()
         {
-            textContainer = new Panel();
-            controlsPanel = new Panel();
+            splitContainer = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.SuspendLayout();
             SuspendLayout();
             // 
-            // textContainer
+            // splitContainer
             // 
-            textContainer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textContainer.BackColor = System.Drawing.Color.Transparent;
-            textContainer.Location = new System.Drawing.Point(320, 0);
-            textContainer.Margin = new Padding(4, 3, 4, 3);
-            textContainer.Name = "textContainer";
-            textContainer.Size = new System.Drawing.Size(710, 412);
-            textContainer.TabIndex = 0;
-            // 
-            // controlsPanel
-            // 
-            controlsPanel.Dock = DockStyle.Left;
-            controlsPanel.Location = new System.Drawing.Point(0, 0);
-            controlsPanel.Margin = new Padding(0);
-            controlsPanel.Name = "controlsPanel";
-            controlsPanel.Size = new System.Drawing.Size(320, 412);
-            controlsPanel.TabIndex = 4;
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Location = new System.Drawing.Point(0, 0);
+            splitContainer.Name = "splitContainer";
+            splitContainer.Size = new System.Drawing.Size(1632, 923);
+            splitContainer.SplitterDistance = 320;
+            splitContainer.TabIndex = 0;
             // 
             // TextControl
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(controlsPanel);
-            Controls.Add(textContainer);
+            Controls.Add(splitContainer);
             Margin = new Padding(0);
             Name = "TextControl";
-            Size = new System.Drawing.Size(1030, 412);
-            controlsPanel.ResumeLayout(false);
-            controlsPanel.PerformLayout();
+            Size = new System.Drawing.Size(1632, 923);
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Panel textContainer;
-        private Panel controlsPanel;
+        private SplitContainer splitContainer;
     }
 }
--- GUI/Controls/TextControl.cs
+++ GUI/Controls/TextControl.cs
@@ -4,14 +4,14 @@ namespace GUI.Controls
 {
     partial class TextControl : ControlPanelView
     {
-        protected override Panel ControlsPanel => controlsPanel;
+        protected override Panel ControlsPanel => splitContainer.Panel1;
         public CodeTextBox TextBox { get; }
         public TextControl()
         {
             InitializeComponent();
             Dock = DockStyle.Fill;
-            TextBox = new CodeTextBox("");
-            textContainer.Controls.Add(TextBox);
+            TextBox = new CodeTextBox(string.Empty);
+            splitContainer.Panel2.Controls.Add(TextBox);
         }
     }
 }
