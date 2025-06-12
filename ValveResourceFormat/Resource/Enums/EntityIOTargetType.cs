5356ff3bdf617094633649f6116286a59d730308
Pavel Djundik <xPaw@users.noreply.github.com>
2025-06-12 23:48:57 +0300

    Add `EntityIOTargetType`

---
+++ ValveResourceFormat/Resource/Enums/EntityIOTargetType.cs
@@ -0,0 +1,13 @@
+namespace ValveResourceFormat;
+
+enum EntityIOTargetType
+{
+    ClassName = 0,
+    ClassNameDerivesFrom = 1,
+    EntityName = 2,
+    ContainsComponent = 3,
+    SpecialActivator = 4,
+    SpecialCaller = 5,
+    EHandle = 6,
+    EntityNameOrClassName = 7,
+}
