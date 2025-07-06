fc643e88e0c58c2ef7339e079571a345f327d8f2
Pavel Djundik <xPaw@users.noreply.github.com>
2025-07-06 22:30:18 +0300

    Validator github runner

--- Misc/ShaderValidator/ShaderValidator.cs
+++ Misc/ShaderValidator/ShaderValidator.cs
@@ -5,7 +5,6 @@ internal class ShaderValidator
         var progressReporter = new Progress<string>(static (str) => GUI.Utils.Log.Info(nameof(ShaderValidator), str));
 
         GUI.Utils.Settings.Load();
-        GUI.Types.Renderer.ShaderParser.ShadersFolderPathOnDisk = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../"));
 
         GUI.Types.Renderer.ShaderLoader.ValidateShadersCore(progressReporter);
         return 0;
