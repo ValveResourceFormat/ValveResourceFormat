edc6ef5d46ed1a9eb864e8bc848d3bbdcec517dd
Pavel Djundik <xPaw@users.noreply.github.com>
2025-07-21 11:15:41 +0300

    Add missing methods to IndentedTextWriter, use inheritdoc
    Only dispose owned writer

--- ValveResourceFormat/Utils/IndentedTextWriter.cs
+++ ValveResourceFormat/Utils/IndentedTextWriter.cs
@@ -16,8 +16,8 @@ namespace ValveResourceFormat
         /// </summary>
         public const string TabString = "\t";
 
-        private readonly StringBuilder builder;
         private readonly StringWriter writer;
+        private readonly bool writerIsOwned;
         private int indentLevel;
         private bool tabsPending;
 
@@ -71,31 +71,53 @@ namespace ValveResourceFormat
         public IndentedTextWriter()
             : base(CultureInfo.InvariantCulture)
         {
-            builder = new StringBuilder();
+            var builder = new StringBuilder();
             writer = new StringWriter(builder, CultureInfo.InvariantCulture);
-            indentLevel = 0;
-            tabsPending = false;
+            writerIsOwned = true;
         }
 
         /// <summary>
-        /// Closes the document being written to.
+        /// Initializes a new instance of the <see cref="IndentedTextWriter"/> class.
         /// </summary>
+        public IndentedTextWriter(StringWriter writer)
+            : base(writer.FormatProvider)
+        {
+            this.writer = writer;
+        }
+
+        /// <inheritdoc/>
         public override void Close()
         {
             writer.Close();
+        }
+
+        /// <inheritdoc/>
+        public override void Flush()
+        {
+            writer.Flush();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && writerIsOwned)
+            {
+                writer.Dispose();
+            }
+
+            base.Dispose(disposing);
         }
 
         /// <summary>
         /// Grows this writer to match the specified min capacity.
         /// </summary>
         /// <param name="minCapacity">The minimum capacity for this writer.</param>
-        public int Grow(int minCapacity) => builder.EnsureCapacity(builder.Length + minCapacity);
+        public int Grow(int minCapacity) => writer.GetStringBuilder().EnsureCapacity(writer.GetStringBuilder().Length + minCapacity);
 
         /// <summary>
         /// Ensures that the capacity of this writer is at least the specified value.
         /// </summary>
         /// <param name="capacity">The new capacity for this writer.</param>
-        public int EnsureCapacity(int capacity) => builder.EnsureCapacity(capacity);
+        public int EnsureCapacity(int capacity) => writer.GetStringBuilder().EnsureCapacity(capacity);
 
         /// <summary>
         /// Outputs the tab string once for each level of indentation according to the <see cref="Indent" /> property.
@@ -115,155 +137,105 @@ namespace ValveResourceFormat
             tabsPending = false;
         }
 
-        /// <summary>
-        /// Writes the specified string to the text stream.
-        /// </summary>
-        /// <param name="value"> The string to write. </param>
+        /// <inheritdoc/>
         public override void Write(string? value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes the text representation of a Boolean value to the text stream.
-        /// </summary>
-        /// <param name="value"> The Boolean value to write. </param>
+        /// <inheritdoc/>
         public override void Write(bool value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes a character to the text stream.
-        /// </summary>
-        /// <param name="value"> The character to write. </param>
+        /// <inheritdoc/>
         public override void Write(char value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes a character array to the text stream.
-        /// </summary>
-        /// <param name="buffer"> The character array to write. </param>
+        /// <inheritdoc/>
         public override void Write(char[]? buffer)
         {
             OutputTabs();
             writer.Write(buffer);
         }
 
-        /// <summary>
-        /// Writes a subarray of characters to the text stream.
-        /// </summary>
-        /// <param name="buffer"> The character array to write data from. </param>
-        /// <param name="index"> Starting index in the buffer. </param>
-        /// <param name="count"> The number of characters to write. </param>
+        /// <inheritdoc/>
         public override void Write(char[] buffer, int index, int count)
         {
             OutputTabs();
             writer.Write(buffer, index, count);
         }
 
-        /// <summary>
-        /// Writes the text representation of a Double to the text stream.
-        /// </summary>
-        /// <param name="value"> The double to write. </param>
+        /// <inheritdoc/>
+        public override void Write(decimal value)
+        {
+            OutputTabs();
+            writer.Write(value);
+        }
+
+        /// <inheritdoc/>
         public override void Write(double value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes the text representation of a Single to the text stream.
-        /// </summary>
-        /// <param name="value"> The single to write. </param>
+        /// <inheritdoc/>
         public override void Write(float value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes the text representation of an integer to the text stream.
-        /// </summary>
-        /// <param name="value"> The integer to write. </param>
+        /// <inheritdoc/>
         public override void Write(int value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes the text representation of an 8-byte integer to the text stream.
-        /// </summary>
-        /// <param name="value"> The 8-byte integer to write. </param>
+        /// <inheritdoc/>
         public override void Write(long value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes the text representation of an object to the text stream.
-        /// </summary>
-        /// <param name="value"> The object to write. </param>
+        /// <inheritdoc/>
         public override void Write(object? value)
         {
             OutputTabs();
             writer.Write(value);
         }
 
-        /// <summary>
-        /// Writes out a formatted string, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string. </param>
-        /// <param name="arg0"> The object to write into the formatted string. </param>
+        /// <inheritdoc/>
         public override void Write([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0)
         {
             OutputTabs();
             writer.Write(format, arg0);
         }
 
-        /// <summary>
-        /// Writes out a formatted string, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string to use. </param>
-        /// <param name="arg0"> The first object to write into the formatted string. </param>
-        /// <param name="arg1"> The second object to write into the formatted string. </param>
+        /// <inheritdoc/>
         public override void Write([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0, object? arg1)
         {
             OutputTabs();
             writer.Write(format, arg0, arg1);
         }
 
-        /// <summary>
-        /// Writes out a formatted string, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string to use. </param>
-        /// <param name="arg"> The argument array to output. </param>
+        /// <inheritdoc/>
         public override void Write([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, params object?[] arg)
         {
             OutputTabs();
             writer.Write(format, arg);
         }
 
-        /// <summary>
-        /// Writes the specified string to a line without tabs.
-        /// </summary>
-        /// <param name="value"> The string to write. </param>
-        public void WriteLineNoTabs(string value)
-        {
-            writer.WriteLine(value);
-        }
-
-        /// <summary>
-        /// Writes the specified string, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The string to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(string? value)
         {
             OutputTabs();
@@ -271,9 +243,15 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes a line terminator.
-        /// </summary>
+        /// <inheritdoc/>
+        public override void WriteLine(ReadOnlySpan<char> buffer)
+        {
+            OutputTabs();
+            writer.WriteLine(buffer);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
         public override void WriteLine()
         {
             OutputTabs();
@@ -281,10 +259,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of a Boolean, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The Boolean to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(bool value)
         {
             OutputTabs();
@@ -292,10 +267,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes a character, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The character to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(char value)
         {
             OutputTabs();
@@ -303,10 +275,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes a character array, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="buffer"> The character array to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(char[]? buffer)
         {
             OutputTabs();
@@ -314,12 +283,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes a subarray of characters, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="buffer"> The character array to write data from. </param>
-        /// <param name="index"> Starting index in the buffer. </param>
-        /// <param name="count"> The number of characters to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(char[] buffer, int index, int count)
         {
             OutputTabs();
@@ -327,10 +291,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of a Double, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The double to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(double value)
         {
             OutputTabs();
@@ -338,10 +299,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of a Single, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The single to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(float value)
         {
             OutputTabs();
@@ -349,10 +307,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of an integer, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The integer to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(int value)
         {
             OutputTabs();
@@ -360,10 +315,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of an 8-byte integer, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The 8-byte integer to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(long value)
         {
             OutputTabs();
@@ -371,10 +323,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes the text representation of an object, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> The object to write. </param>
+        /// <inheritdoc/>
         public override void WriteLine(object? value)
         {
             OutputTabs();
@@ -382,47 +331,7 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
-        /// <summary>
-        /// Writes out a formatted string, followed by a line terminator, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string. </param>
-        /// <param name="arg0"> The object to write into the formatted string. </param>
-        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0)
-        {
-            OutputTabs();
-            writer.WriteLine(format, arg0);
-            tabsPending = true;
-        }
-
-        /// <summary>
-        /// Writes out a formatted string, followed by a line terminator, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string to use. </param>
-        /// <param name="arg0"> The first object to write into the formatted string. </param>
-        /// <param name="arg1"> The second object to write into the formatted string. </param>
-        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0, object? arg1)
-        {
-            OutputTabs();
-            writer.WriteLine(format, arg0, arg1);
-            tabsPending = true;
-        }
-
-        /// <summary>
-        /// Writes out a formatted string, followed by a line terminator, using the same semantics as specified.
-        /// </summary>
-        /// <param name="format"> The formatting string to use. </param>
-        /// <param name="arg"> The argument array to output. </param>
-        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, params object?[] arg)
-        {
-            OutputTabs();
-            writer.WriteLine(format, arg);
-            tabsPending = true;
-        }
-
-        /// <summary>
-        /// Writes the text representation of a UInt32, followed by a line terminator, to the text stream.
-        /// </summary>
-        /// <param name="value"> A UInt32 to output. </param>
+        /// <inheritdoc/>
         public override void WriteLine(uint value)
         {
             OutputTabs();
@@ -430,6 +339,55 @@ namespace ValveResourceFormat
             tabsPending = true;
         }
 
+        /// <inheritdoc/>
+        public override void WriteLine(ulong value)
+        {
+            OutputTabs();
+            writer.WriteLine(value);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0)
+        {
+            OutputTabs();
+            writer.WriteLine(format, arg0);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0, object? arg1)
+        {
+            OutputTabs();
+            writer.WriteLine(format, arg0, arg1);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, object? arg0, object? arg1, object? arg2)
+        {
+            OutputTabs();
+            writer.WriteLine(format, arg0, arg1, arg2);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, params object?[] arg)
+        {
+            OutputTabs();
+            writer.WriteLine(format, arg);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteLine([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format, params ReadOnlySpan<object?> arg)
+        {
+            OutputTabs();
+            writer.WriteLine(format, arg);
+            tabsPending = true;
+        }
+
+        /// <inheritdoc/>
         public override string ToString()
         {
             return writer.ToString();
