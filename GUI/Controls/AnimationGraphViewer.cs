859dd6cd247bed529d4f120d001a80f0e083db69
kristiker <kristik06@outlook.com>
2025-08-06 15:45:39 +0200

    Create input nodes for root node

--- GUI/Controls/AnimationGraphViewer.cs
+++ GUI/Controls/AnimationGraphViewer.cs
@@ -37,17 +37,60 @@ internal class AnimationGraphViewer : NodeGraphControl.NodeGraphControl
         string GetType(int nodeIdx)
         {
             var className = nodes[nodeIdx].GetStringProperty("_class");
-            var @type = className["CNm".Length..^"Node::CDefinition".Length];
+            const string Preffix = "CNm";
+            const string Suffix = "Node::CDefinition";
+            var @type = className[Preffix.Length..^Suffix.Length];
             return @type;
         }
 
-        AddNode(new Node
+        void HandleNode(Node node, int nodeIdx)
+        {
+            var data = nodes[nodeIdx];
+
+            if (node.NodeType == "StateMachine")
+            {
+                foreach (var stateDefinition in data.GetArray("m_stateDefinitions"))
+                {
+                    var stateNodeIdx = stateDefinition.GetInt32Property("m_nStateNodeIdx");
+                    var entryConditionNodeIdx = stateDefinition.GetInt32Property("m_nEntryConditionNodeIdx");
+                    // m_transitionDefinitions
+
+                    var stateName = GetName(stateNodeIdx);
+                    var stateNode = nodes[stateNodeIdx];
+                    var stateInputIdx = stateNode.GetInt32Property("m_nChildNodeIdx");
+
+                    Console.WriteLine($"State: {GetName(stateNodeIdx)}, Entry Condition: {GetName(entryConditionNodeIdx)}");
+
+                    var inputSocket = new SocketIn(typeof(int), stateName, node, false);
+                    node.Sockets.Add(inputSocket);
+
+                    var stateInputNode = AddNode(new Node
+                    {
+                        Name = GetName(stateInputIdx),
+                        Location = new Point(0, 0),
+                        NodeType = GetType(stateInputIdx),
+                    });
+
+
+                    var outputSocket = new SocketOut(typeof(int), "Output", stateInputNode);
+                    stateInputNode.Sockets.Add(outputSocket);
+
+                    Connect(outputSocket, inputSocket);
+                }
+            }
+        }
+
+        var root = AddNode(new Node
         {
             Name = GetName(rootNodeIdx),
             Location = new Point(0, 0),
             NodeType = GetType(rootNodeIdx),
             Description = "Root node"
         });
+
+        root.StartNode = true;
+
+        HandleNode(root, rootNodeIdx);
     }
 
     #region Nodes
