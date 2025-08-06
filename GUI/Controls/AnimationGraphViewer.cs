c03aba094ceae9db6d7ed7cf757a3ecb71271931
kristiker <kristik06@outlook.com>
2025-08-06 14:22:20 +0200

    Create root node test

--- GUI/Controls/AnimationGraphViewer.cs
+++ GUI/Controls/AnimationGraphViewer.cs
@@ -1,6 +1,7 @@
-
+using System.Drawing;
+using System.Windows.Forms;
 using GUI.Utils;
-using NodeGraphControl;
+using NodeGraphControl.Elements;
 using ValveResourceFormat.Serialization.KeyValues;
 
 namespace GUI.Types.Viewers;
@@ -8,11 +9,59 @@ namespace GUI.Types.Viewers;
 internal class AnimationGraphViewer : NodeGraphControl.NodeGraphControl
 {
     private VrfGuiContext vrfGuiContext;
-    private KVObject animationGraphDefinition;
+    private KVObject graphDefinition;
 
     public AnimationGraphViewer(VrfGuiContext guiContext, KVObject data)
     {
+        Dock = DockStyle.Fill;
+        GridStyle = EGridStyle.Grid;
+        // BackColor =
+
         vrfGuiContext = guiContext;
-        animationGraphDefinition = data;
+        graphDefinition = data;
+
+        CreateGraph();
     }
+
+    private void CreateGraph()
+    {
+        var rootNodeIdx = graphDefinition.GetInt32Property("m_nRootNodeIdx");
+        var nodePaths = graphDefinition.GetArray<string>("m_nodePaths");
+        var nodes = graphDefinition.GetArray("m_nodes");
+
+        string GetName(int nodeIdx)
+        {
+            return nodePaths[nodeIdx].Split('/')[^1];
+        }
+
+        string GetType(int nodeIdx)
+        {
+            var className = nodes[nodeIdx].GetStringProperty("_class");
+            var @type = className["CNm".Length..^"Node::CDefinition".Length];
+            return @type;
+        }
+
+        AddNode(new Node
+        {
+            Name = GetName(rootNodeIdx),
+            Location = new Point(0, 0),
+            NodeType = GetType(rootNodeIdx),
+            Description = "Root node"
+        });
+    }
+
+    #region Nodes
+
+    class Node : AbstractNode
+    {
+        public Node()
+        {
+            BaseColor = Color.FromArgb(255, 31, 36, 42);
+        }
+
+        public override bool IsReady() => true;
+        public override void Execute() { }
+    }
+
+    #endregion Nodes
 }
