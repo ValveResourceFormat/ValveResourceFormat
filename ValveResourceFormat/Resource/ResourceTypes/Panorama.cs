ValveResourceFormat/ValveResourceFormat

commit 7cc9cdf53987183ff07a78b9259bef199fefb83e

Serialize plaintext blocks

ValveResourceFormat/Resource/ResourceTypes/Panorama.cs
@@ -26,8 +26,7 @@ namespace ValveResourceFormat.ResourceTypes
         {
             reader.BaseStream.Position = Offset;
 
-            if ((Resource.ResourceType == ResourceType.PanoramaScript && Resource.Version >= 4)
-            || (Resource.ResourceType == ResourceType.PanoramaTypescript && Resource.Version >= 2))
+            if (IsPlaintext())
             {
                 Data = reader.ReadBytes((int)Size);
 
@@ -64,12 +63,33 @@ namespace ValveResourceFormat.ResourceTypes
 
         public override void Serialize(Stream stream)
         {
+            if (IsPlaintext())
+            {
+                stream.Write(Data);
+                return;
+            }
+
             throw new NotImplementedException("Serializing this block is not yet supported. If you need this, send us a pull request!");
         }
 
         public override void WriteText(IndentedTextWriter writer)
         {
-            writer.Write(Encoding.UTF8.GetString(Data));
+            writer.Write(Data);
+        }
+
+        private bool IsPlaintext()
+        {
+            if (Resource.ResourceType == ResourceType.PanoramaScript && Resource.Version >= 4)
+            {
+                return true;
+            }
+
+            if (Resource.ResourceType == ResourceType.PanoramaTypescript && Resource.Version >= 2)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }

Tests/Test.cs
@@ -80,6 +80,8 @@ namespace Tests
             {
                 RecurseSubdirectories = true,
             });
+            var total = 0;
+            var notImplemented = 0;
 
             if (files.Length == 0)
             {
@@ -104,12 +106,15 @@ namespace Tests
                         continue;
                     }
 
+                    total++;
+
                     try
                     {
                         resourceOnDisk.Serialize(ms);
                     }
                     catch (NotImplementedException)
                     {
+                        notImplemented++;
                         continue;
                     }
                 }
@@ -145,6 +150,8 @@ namespace Tests
             }
 
             VerifyResources(resources, validateMissingResources: false);
+
+            Console.WriteLine($"{notImplemented} out of {total} files are not yet serializable.");
         }
 
         [Test]

ValveResourceFormat/Resource/ResourceTypes/Plaintext.cs
@@ -20,7 +20,7 @@ namespace ValveResourceFormat.ResourceTypes
 
         public override void Serialize(Stream stream)
         {
-            throw new NotImplementedException("Serializing this block is not yet supported. If you need this, send us a pull request!");
+            stream.Write(Encoding.UTF8.GetBytes(Data));
         }
 
         public override void WriteText(IndentedTextWriter writer)

commit e45857eb4bf2d32e713a4047005d2253a8912c30

Fix writing panorama to text
Broken in 7cc9cdf53987183ff07a78b9259bef199fefb83e

ValveResourceFormat/Resource/ResourceTypes/Panorama.cs
@@ -74,7 +74,7 @@ namespace ValveResourceFormat.ResourceTypes
 
         public override void WriteText(IndentedTextWriter writer)
         {
-            writer.Write(Data);
+            writer.Write(Encoding.UTF8.GetString(Data));
         }
 
         private bool IsPlaintext()

ValveResourceFormat/Utils/IndentedTextWriter.cs
@@ -208,7 +208,10 @@ namespace ValveResourceFormat
         }
 
         /// <inheritdoc/>
+        [Obsolete("Do not write to string as a generic object, this is probably a mistake.")]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
         public override void Write(object? value)
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
         {
             OutputTabs();
             writer.Write(value);