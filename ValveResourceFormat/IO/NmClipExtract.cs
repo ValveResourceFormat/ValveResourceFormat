2b2f2e7c2bb0bc058eb586e7a95fae9ca5994097
Krystian <KrystianoXPL@gmail.com>
2025-09-20 16:59:41 +0200

    Handle NmClip events in a more generic way

--- ValveResourceFormat/IO/NmClipExtract.cs
+++ ValveResourceFormat/IO/NmClipExtract.cs
@@ -60,10 +60,11 @@ public class NmClipExtract
             var docEventTrack = BuildDocEventBasedOnEventClass(ev, ev.GetStringProperty("_class"));
             var startTimeSeconds = ev.GetFloatProperty("m_flStartTimeSeconds");
             var durationSeconds = ev.GetFloatProperty("m_flDurationSeconds");
+            var eventList = docEventTrack.GetArray<KVObject>("m_events").First();
             // Doc file event time stamps are given in frames they can be technically floats, but based on recompilation tests
             // these seem inconsistent, unless they're floored to int, then it matches up.
-            docEventTrack.GetArray<KVObject>("m_events").First().AddProperty("m_flStartTime", Math.Floor(startTimeSeconds * animation.Fps));
-            docEventTrack.GetArray<KVObject>("m_events").First().AddProperty("m_flDuration", Math.Floor(durationSeconds * animation.Fps));
+            eventList.AddProperty("m_flStartTime", Math.Floor(startTimeSeconds * animation.Fps));
+            eventList.AddProperty("m_flDuration", Math.Floor(durationSeconds * animation.Fps));
             docEventTracks.AddItem(docEventTrack);
         }
         kv.AddProperty("m_eventTracks", docEventTracks);
@@ -78,144 +79,73 @@ public class NmClipExtract
         // even though m_events is an array inside each track.
         var kvDocEventTrack = new KVObject(null);
         var kvDocEvent = new KVObject("m_event");
-        kvDocEventTrack.AddProperty("m_type", "Duration");
-        kvDocEventTrack.AddProperty("m_bIsSyncTrack", kvCompiledEvent.ContainsKey("m_syncID"));
-        switch (className)
-        {
-            case "CNmIDEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_ID");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_ID");
-                    kvDocEvent.AddProperty("m_ID", kvCompiledEvent.GetStringProperty("m_ID"));
-                    kvDocEvent.AddProperty("m_secondaryID", kvCompiledEvent.GetStringProperty("m_secondaryID"));
-                    break;
-                }
-            case "CNmEntityAttributeIntEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_EntityAttribute");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_EntityAttribute");
-                    kvDocEvent.AddProperty("m_attributeName", kvCompiledEvent.GetStringProperty("m_attributeName"));
-                    kvDocEvent.AddProperty("m_nValueType", "EVENT_ENTITY_ATTR_TYPE_INT");
-                    kvDocEvent.AddProperty("m_nIntValue", kvCompiledEvent.GetInt32Property("m_nIntValue"));
-                    break;
-                }
-            case "CNmEntityAttributeFloatEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_EntityAttribute");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_EntityAttribute");
-                    kvDocEvent.AddProperty("m_attributeName", kvCompiledEvent.GetStringProperty("m_attributeName"));
-                    kvDocEvent.AddProperty("m_nValueType", "EVENT_ENTITY_ATTR_TYPE_FLOAT");
-                    kvDocEvent.AddProperty("m_FloatValue", kvCompiledEvent.GetProperty<object>("m_FloatValue"));
-                    break;
-                }
 
-            case "CNmFloatCurveEvent":
+        kvDocEventTrack.AddProperty("m_type", "Duration"); // Doesn't seem to matter?
+        kvDocEventTrack.AddProperty("m_bIsSyncTrack", kvCompiledEvent.ContainsKey("m_syncID"));
+        // Get string between "CNm" and "Event".
+        var eventName = className[3..^5];
+        // Example: CNmIDEvent maps to CNmClipDocEvent_ID.
+        var docEventClass = "CNmClipDocEvent_" + eventName;
+        kvDocEventTrack.AddProperty("m_eventClassName", docEventClass);
+        kvDocEvent.AddProperty("_class", docEventClass);
+        foreach (var field in kvCompiledEvent)
+        {
+            // These were already handled and shouldn't be copied over.
+            if (field.Key == "m_flStartTimeSeconds" || field.Key == "m_flDurationSeconds")
+            {
+                continue;
+            }
+            // Handle special cases - a few doc fields are different to the compiled event definition.
+            switch (eventName)
+            {
+                case "Particle":
+                    {
+                        if (field.Key == "m_hParticleSystem")
+                        {
+                            kvDocEvent.AddProperty("m_particleSystem", field.Value);
+                        }
+                    }
+                    break;
+                case "Legacy":
+                    {
+                        if (field.Key == "m_animEventClassName")
+                        {
+                            kvDocEvent.AddProperty("m_eventClass", field.Value);
+                        }
+                        break;
+                    }
+                case "Transition":
+                    {
+                        if (field.Key == "m_ID")
+                        {
+                            kvDocEvent.AddProperty("m_optionalID", field.Value);
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        kvDocEvent.AddProperty(field.Key, field.Value);
+                        break;
+                    }
+            }
+        }
+        // Handle special cases - only additional fields that need adding afterwards.
+        switch (eventName)
+        {
+            case "EntityAttributeInt":
                 {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_FloatCurve");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_FloatCurve");
-                    kvDocEvent.AddProperty("m_curve", kvCompiledEvent.GetProperty<object>("m_curve"));
-                    kvDocEvent.AddProperty("m_ID", kvCompiledEvent.GetStringProperty("m_ID"));
+                    kvDocEvent.AddProperty("m_nValueType", "EVENT_ENTITY_ATTR_TYPE_INT");
                     break;
                 }
-            case "CNmFootEvent":
+            case "EntityAttributeFloat":
                 {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_Foot");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_Foot");
-                    kvDocEvent.AddProperty("m_phase", kvCompiledEvent.GetProperty<object>("m_phase"));
-                    break;
-                }
-            case "CNmFrameSnapEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_FrameSnap");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_FrameSnap");
-                    kvDocEvent.AddProperty("m_frameSnapMode", kvCompiledEvent.GetProperty<object>("m_frameSnapMode"));
-                    break;
-                }
-            case "CNmLegacyEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_Legacy");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_Legacy");
-                    kvDocEvent.AddProperty("m_eventClass", kvCompiledEvent.GetStringProperty("m_animEventClassName"));
-                    kvDocEvent.AddProperty("m_KV", kvCompiledEvent.GetProperty<KVObject>("m_KV"));
-                    break;
-                }
-            case "CNmMaterialAttributeEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_MaterialAttribute");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_MaterialAttribute");
-                    kvDocEvent.AddProperty("m_attributeName", kvCompiledEvent.GetStringProperty("m_attributeName"));
-                    kvDocEvent.AddProperty("m_x", kvCompiledEvent.GetProperty<object>("m_x"));
-                    kvDocEvent.AddProperty("m_y", kvCompiledEvent.GetProperty<object>("m_y"));
-                    kvDocEvent.AddProperty("m_z", kvCompiledEvent.GetProperty<object>("m_z"));
-                    kvDocEvent.AddProperty("m_w", kvCompiledEvent.GetProperty<object>("m_w"));
-                    break;
-                }
-            case "CNmOrientationWarpEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_OrientationWarp");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_OrientationWarp");
-                    break;
-                }
-            case "CNmParticleEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_Particle");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_Particle");
-                    kvDocEvent.AddProperty("m_relevance", kvCompiledEvent.GetStringProperty("m_relevance"));
-                    kvDocEvent.AddProperty("m_type", kvCompiledEvent.GetStringProperty("m_type"));
-                    kvDocEvent.AddProperty("m_particleSystem", kvCompiledEvent.GetStringProperty("m_hParticleSystem"));
-                    kvDocEvent.AddProperty("m_bDetachFromOwner", kvCompiledEvent.GetProperty<bool>("m_bDetachFromOwner"));
-                    kvDocEvent.AddProperty("m_bStopImmediately", kvCompiledEvent.GetProperty<bool>("m_bStopImmediately"));
-                    kvDocEvent.AddProperty("m_bPlayEndCap", kvCompiledEvent.GetProperty<bool>("m_bPlayEndCap"));
-                    kvDocEvent.AddProperty("m_attachmentPoint0", kvCompiledEvent.GetStringProperty("m_attachmentPoint0"));
-                    kvDocEvent.AddProperty("m_attachmentType0", kvCompiledEvent.GetStringProperty("m_attachmentType0"));
-                    kvDocEvent.AddProperty("m_attachmentPoint1", kvCompiledEvent.GetStringProperty("m_attachmentPoint1"));
-                    kvDocEvent.AddProperty("m_attachmentType1", kvCompiledEvent.GetStringProperty("m_attachmentType1"));
-                    kvDocEvent.AddProperty("m_config", kvCompiledEvent.GetStringProperty("m_config"));
-                    kvDocEvent.AddProperty("m_effectForConfig", kvCompiledEvent.GetStringProperty("m_effectForConfig"));
-                    kvDocEvent.AddProperty("m_tags", kvCompiledEvent.GetStringProperty("m_tags"));
-                    break;
-                }
-            case "CNmRootMotionEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_RootMotion");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_RootMotion");
-                    kvDocEvent.AddProperty("m_flBlendTimeSeconds", kvCompiledEvent.GetFloatProperty("m_flBlendTimeSeconds"));
-                    break;
-                }
-            case "CNmSoundEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_Sound");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_Sound");
-                    kvDocEvent.AddProperty("m_relevance", kvCompiledEvent.GetStringProperty("m_relevance"));
-                    kvDocEvent.AddProperty("m_type", kvCompiledEvent.GetStringProperty("m_type"));
-                    kvDocEvent.AddProperty("m_bContinuePlayingSoundAtDurationEnd", kvCompiledEvent.GetProperty<bool>("m_bContinuePlayingSoundAtDurationEnd"));
-                    kvDocEvent.AddProperty("m_flDurationInterruptionThreshold", kvCompiledEvent.GetFloatProperty("m_flDurationInterruptionThreshold"));
-                    kvDocEvent.AddProperty("m_name", kvCompiledEvent.GetStringProperty("m_name"));
-                    kvDocEvent.AddProperty("m_position", kvCompiledEvent.GetStringProperty("m_position"));
-                    kvDocEvent.AddProperty("m_attachmentName", kvCompiledEvent.GetStringProperty("m_attachmentName"));
-                    kvDocEvent.AddProperty("m_tags", kvCompiledEvent.GetStringProperty("m_tags"));
-                    break;
-                }
-            case "CNmTargetWarpEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_TargetWarp");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_TargetWarp");
-                    kvDocEvent.AddProperty("m_rule", kvCompiledEvent.GetStringProperty("m_rule"));
-                    kvDocEvent.AddProperty("m_algorithm", kvCompiledEvent.GetStringProperty("m_algorithm"));
-                    break;
-                }
-            case "CNmTransitionEvent":
-                {
-                    kvDocEventTrack.AddProperty("m_eventClassName", "CNmClipDocEvent_Transition");
-                    kvDocEvent.AddProperty("_class", "CNmClipDocEvent_Transition");
-                    kvDocEvent.AddProperty("m_rule", kvCompiledEvent.GetStringProperty("m_rule"));
-                    kvDocEvent.AddProperty("m_optionalID", kvCompiledEvent.GetStringProperty("m_ID"));
+                    kvDocEvent.AddProperty("m_nValueType", "EVENT_ENTITY_ATTR_TYPE_FLOAT");
                     break;
                 }
         }
-        var arr = new KVObject("m_events", true, 1);
-        arr.AddItem(kvDocEvent);
-        kvDocEventTrack.AddProperty("m_events", arr);
+        var eventsArray = new KVObject("m_events", true, 1);
+        eventsArray.AddItem(kvDocEvent);
+        kvDocEventTrack.AddProperty("m_events", eventsArray);
         return kvDocEventTrack;
     }
 }
