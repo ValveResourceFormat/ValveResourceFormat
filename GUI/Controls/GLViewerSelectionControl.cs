204a213167d3a2b6dcac70e1f7933fbb06f90f33
Kristi K <kristik06@outlook.com>
2025-11-03 19:43:09 +0100

    Fix combo box size for flat GLViewerSelectionControl

--- GUI/Controls/GLViewerSelectionControl.cs
+++ GUI/Controls/GLViewerSelectionControl.cs
@@ -18,33 +18,45 @@ namespace GUI.Controls
             : this()
         {
             SuspendLayout();
-            selectionNameLabel.Text = $"{name}:";
 
             if (horizontal)
             {
+                selectionNameLabel.Text = name;
                 Controls.Clear();
 
                 layoutPanel = new TableLayoutPanel
                 {
                     Dock = fill ? DockStyle.Fill : DockStyle.Top,
-                    AutoSize = false,
+                    AutoSize = !fill,
                     ColumnCount = 2,
                     Padding = new Padding(1, 2, 1, 2),
                 };
+
+                layoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));  // Label column
+                layoutPanel.ColumnStyles.Add(fill
+                    ? new ColumnStyle(SizeType.Percent, 100F)
+                    : new ColumnStyle(SizeType.Absolute)
+                );
 
                 selectionNameLabel.AutoSize = true;
                 selectionNameLabel.Margin = new Padding(0, 4, 5, 0);
                 selectionNameLabel.Anchor = AnchorStyles.Left | AnchorStyles.Top;
 
-                comboBox.Anchor = AnchorStyles.Left | AnchorStyles.Top;
-                comboBox.AutoSize = true;  // Let it size to content
+                comboBox.AutoSize = !fill;
+                comboBox.Anchor = fill
+                    ? (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right)
+                    : (AnchorStyles.Left | AnchorStyles.Top);
 
                 layoutPanel.Controls.Add(selectionNameLabel, 0, 0);
                 layoutPanel.Controls.Add(comboBox, 1, 0);
 
                 Controls.Add(layoutPanel);
-                MinimumSize = new Size(selectionNameLabel.Width + 20, comboBox.Height + layoutPanel.Padding.Vertical);
+                MinimumSize = new Size(selectionNameLabel.Width, comboBox.Height + layoutPanel.Padding.Vertical);
                 Size = new Size(Size.Width, MinimumSize.Height);
+            }
+            else
+            {
+                selectionNameLabel.Text = $"{name}:";
             }
 
             ResumeLayout();
