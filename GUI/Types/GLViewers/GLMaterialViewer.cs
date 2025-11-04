2dc59cf3f520c3f3de0a972e48aed61d774e230c
Kristi K <kristik06@outlook.com>
2025-11-04 20:15:44 +0100

    Material Parameters Table: Suspend layout while creating

--- GUI/Types/GLViewers/GLMaterialViewer.cs
+++ GUI/Types/GLViewers/GLMaterialViewer.cs
@@ -245,6 +245,7 @@ namespace GUI.Types.GLViewers
                 .ToList();
 
             var currentHeading = string.Empty;
+            ParamsTable.SuspendLayout();
 
             // Add parameters to UI with layer headers
             foreach (var (paramName, value, type, vfxDescription) in sortedParams)
@@ -328,6 +329,8 @@ namespace GUI.Types.GLViewers
                         break;
                 }
             }
+
+            ParamsTable.ResumeLayout();
         }
 
         private void AddBooleanParameter(string paramName, bool initialValue, Action<bool> onValueChanged, bool isEnabled = true)
