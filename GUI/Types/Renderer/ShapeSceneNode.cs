8e7541f81f514b2ec2e33e94c49cdf2bceae3f26
Kristi K <kristik06@outlook.com>
2025-11-06 18:11:46 +0100

    Fix triangle gaps in physics visualization

--- GUI/Types/Renderer/ShapeSceneNode.cs
+++ GUI/Types/Renderer/ShapeSceneNode.cs
@@ -303,6 +303,8 @@ namespace GUI.Types.Renderer
 
             GL.BindVertexArray(vaoHandle);
 
+            GL.DepthFunc(DepthFunction.Gequal);
+
             if (isTranslucent)
             {
                 GL.Disable(EnableCap.CullFace);
@@ -318,7 +320,7 @@ namespace GUI.Types.Renderer
                 GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
                 GL.Enable(EnableCap.PolygonOffsetLine);
                 GL.Enable(EnableCap.PolygonOffsetFill);
-                GL.PolygonOffsetClamp(0, 100, 0.005f);
+                GL.PolygonOffsetClamp(2, 100, 0.05f);
 
                 GL.DrawElements(PrimitiveType.Triangles, indexCount, DrawElementsType.UnsignedInt, 0);
 
@@ -331,6 +333,8 @@ namespace GUI.Types.Renderer
             {
                 GL.DrawElements(PrimitiveType.Triangles, indexCount, DrawElementsType.UnsignedInt, 0);
             }
+
+            GL.DepthFunc(DepthFunction.Greater);
 
             GL.UseProgram(0);
             GL.BindVertexArray(0);
