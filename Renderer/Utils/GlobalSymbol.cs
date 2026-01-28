e3f26dd6a569f0b7ad0a5f9987756305c1c7c455
kristiker <kristik06@outlook.com>
2026-01-28 11:39:23 +0100

    Create GlobalSymbol

---
+++ Renderer/Utils/GlobalSymbol.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+
+namespace ValveResourceFormat.Renderer;
+
+/// <summary>
+/// String token hash entry.
+/// </summary>
+readonly struct GlobalSymbol : IEquatable<GlobalSymbol>
+{
+    public string Name => StringToken.GetKnownString(Token);
+    public uint Token { get; }
+
+    public GlobalSymbol(string name)
+    {
+        Token = StringToken.Store(name);
+    }
+
+    public GlobalSymbol(uint token)
+    {
+        Debug.Assert(
+            StringToken.InvertedTable.ContainsKey(token),
+            $"Unknown symbol {token}."
+        );
+
+        Token = token;
+    }
+
+    public override readonly string ToString() => Name;
+
+    public static implicit operator uint(GlobalSymbol symbol) => symbol.Token;
+    public static implicit operator GlobalSymbol(string name) => new(name);
+
+    // record struct?
+
+    public bool Equals(GlobalSymbol other) => Token == other.Token;
+    public override int GetHashCode() => (int)Token;
+
+    public override bool Equals(object? obj)
+    {
+        Debug.Assert(false, $"Boxing operation on {nameof(GlobalSymbol)}. Can you avoid this?");
+        return obj is GlobalSymbol other && Equals(other);
+    }
+
+    public static bool operator ==(GlobalSymbol left, GlobalSymbol right) => left.Equals(right);
+    public static bool operator !=(GlobalSymbol left, GlobalSymbol right) => !left.Equals(right);
+}
