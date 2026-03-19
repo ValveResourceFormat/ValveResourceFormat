d5ad0c5c146d40f9bff9651dcd88c78465955975
Angel <angelcazacu8@gmail.com>
2026-03-19 12:29:28 +0100

    fix model phys rendering

--- GUI/Types/PackageViewer/ThumbnailRenderers/ThumbnailModelRenderer.cs
+++ GUI/Types/PackageViewer/ThumbnailRenderers/ThumbnailModelRenderer.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Linq;
 using ValveResourceFormat;
 using ValveResourceFormat.Renderer.SceneNodes;
 using ValveResourceFormat.ResourceTypes;
@@ -10,16 +11,37 @@ internal class ThumbnailModelRenderer : ThumbnailRenderer
     public override void SetResource(Resource resource)
     {
         var model = (Model)resource.DataBlock!;
+        PhysAggregateData? phys = null;
 
         Debug.Assert(SceneRenderer != null);
 
         var modelSceneNode = new ModelSceneNode(SceneRenderer.Scene, model);
         SceneRenderer.Scene.Add(modelSceneNode, true);
 
-        var bbox = modelSceneNode.BoundingBox;
+        // if model has no meshes try to show physics
+        if (modelSceneNode == null || modelSceneNode.RenderableMeshes.Count == 0)
+        {
+            phys = model.GetEmbeddedPhys();
+
+            if (phys != null)
+            {
+                var physSceneNodes = PhysSceneNode.CreatePhysSceneNodes(SceneRenderer.Scene, phys, null).ToList();
+
+                foreach (var physSceneNode in physSceneNodes)
+                {
+                    physSceneNode.Enabled = true;
+                    physSceneNode.IsTranslucentRenderMode = false;
+                    SceneRenderer.Scene.Add(physSceneNode, false);
+                }
+            }
+        }
+
+        var bbox = SceneRenderer.Scene.AllNodes
+        .Select(n => n.BoundingBox)
+        .Aggregate((a, b) => a.Union(b));
 
         // add some padding
-        var size = bbox.Size * 1.5f;
+        var size = bbox.Size * (phys == null ? 1.5f : 1);
 
         SceneRenderer.Camera.RecalculateDirectionVectors();
         SceneRenderer.Camera.FrameObject(bbox.Center, size.X, size.Z, size.Y);
