44e383639b5af845ef22c7c787fe83e7823f8969
Angel <angelcazacu8@gmail.com>
2026-03-19 13:28:45 +0100

    account for texture aspect ratio

--- GUI/Types/PackageViewer/ThumbnailRenderers/ThumbnailTextureRenderer.cs
+++ GUI/Types/PackageViewer/ThumbnailRenderers/ThumbnailTextureRenderer.cs
@@ -38,8 +38,39 @@ internal class ThumbnailTextureRenderer : ThumbnailRenderer
         }
 
         using var bitmap = textureData.GenerateBitmap(mipLevel: (uint)mipLevel);
-        using var resizedBitmap = bitmap.Resize(new SKSizeI(size, size), SKSamplingOptions.Default);
 
-        return resizedBitmap.ToBitmap();
+        var originalWidth = bitmap.Width;
+        var originalHeight = bitmap.Height;
+
+        int renderWidth, renderHeight;
+
+        if (originalWidth <= 0 || originalHeight <= 0)
+        {
+            renderWidth = size;
+            renderHeight = size;
+        }
+        else
+        {
+            var scale = Math.Min((float)size / originalWidth, (float)size / originalHeight);
+            renderWidth = (int)Math.Round(originalWidth * scale);
+            renderHeight = (int)Math.Round(originalHeight * scale);
+        }
+
+        using var resizedBitmap = bitmap.Resize(new SKSizeI(renderWidth, renderHeight), SKSamplingOptions.Default);
+
+        if (renderWidth == size && renderHeight == size)
+        {
+            return resizedBitmap.ToBitmap();
+        }
+
+        using var surface = SKSurface.Create(new SKImageInfo(size, size, resizedBitmap.ColorType, resizedBitmap.AlphaType));
+        var canvas = surface.Canvas;
+        var offsetX = (size - renderWidth) / 2;
+        var offsetY = (size - renderHeight) / 2;
+        canvas.DrawBitmap(resizedBitmap, offsetX, offsetY);
+
+        using var snapshot = surface.Snapshot();
+        using var finalBitmap = SKBitmap.FromImage(snapshot);
+        return finalBitmap.ToBitmap();
     }
 }
