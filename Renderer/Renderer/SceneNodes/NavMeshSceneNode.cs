68c09e84766054bedad0ce9c74af91ded2eef524
Pavel Djundik <xPaw@users.noreply.github.com>
2026-08-15 23:12:48 +0300

    Draw nav meshes with triangle and line lists instead of fans, loops and primitive restart

--- Renderer/Renderer/SceneNodes/NavMeshSceneNode.cs
+++ Renderer/Renderer/SceneNodes/NavMeshSceneNode.cs
@@ -11,17 +11,45 @@ namespace ValveResourceFormat.Renderer.SceneNodes
         private static readonly Color32 NavMeshColor = new(64, 32, 255, 100);
         private static readonly Color32 NavMeshLadderColor = new(16, 255, 32, 100);
 
+        private readonly int triangleIndexCount;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="NavMeshSceneNode"/> class from pre-built vertex and index lists.
         /// </summary>
         /// <param name="scene">The scene this node belongs to.</param>
         /// <param name="verts">The vertex data for the nav mesh geometry.</param>
-        /// <param name="inds">The index data for the nav mesh geometry.</param>
-        public NavMeshSceneNode(Scene scene, List<SimpleVertexNormal> verts, List<int> inds) : base(scene, verts, inds)
+        /// <param name="triangleInds">Triangle list indices for the filled areas.</param>
+        /// <param name="lineInds">Line list indices for the area outlines.</param>
+        public NavMeshSceneNode(Scene scene, List<SimpleVertexNormal> verts, List<int> triangleInds, List<int> lineInds)
+            : base(scene, verts, [.. triangleInds, .. lineInds])
         {
+            triangleIndexCount = triangleInds.Count;
         }
 
-        private static void AddLadder(NavMeshLadder ladder, List<SimpleVertexNormal> verts, List<int> inds, Color32 color, ref Vector3 minBounds, ref Vector3 maxBounds)
+        // Corners form a convex polygon in fan order: fill as a triangle list, outline as a line list.
+        private static void AddPolygon(int firstVertexIndex, int cornerCount, List<int> triangleInds, List<int> lineInds)
+        {
+            if (cornerCount < 3)
+            {
+                return;
+            }
+
+            triangleInds.EnsureCapacity(triangleInds.Count + (cornerCount - 2) * 3);
+            lineInds.EnsureCapacity(lineInds.Count + cornerCount * 2);
+
+            for (var i = 1; i < cornerCount - 1; i++)
+            {
+                AddTriangle(triangleInds, firstVertexIndex, 0, i, i + 1);
+            }
+
+            for (var i = 0; i < cornerCount; i++)
+            {
+                lineInds.Add(firstVertexIndex + i);
+                lineInds.Add(firstVertexIndex + (i + 1) % cornerCount);
+            }
+        }
+
+        private static void AddLadder(NavMeshLadder ladder, List<SimpleVertexNormal> verts, List<int> triangleInds, List<int> lineInds, Color32 color, ref Vector3 minBounds, ref Vector3 maxBounds)
         {
             var normal = ladder.Direction switch
             {
@@ -49,19 +77,14 @@ namespace ValveResourceFormat.Renderer.SceneNodes
             verts.Add(new(top1, color, normal));
             verts.Add(new(top2, color, normal));
 
-            for (var i = 0; i < 4; i++)
-            {
-                inds.Add(firstVertexIndex + i);
-            }
-            inds.Add(int.MaxValue);
+            AddPolygon(firstVertexIndex, 4, triangleInds, lineInds);
         }
 
-        private static void AddArea(NavMeshArea area, List<SimpleVertexNormal> verts, List<int> inds, Color32 color, ref Vector3 minBounds, ref Vector3 maxBounds)
+        private static void AddArea(NavMeshArea area, List<SimpleVertexNormal> verts, List<int> triangleInds, List<int> lineInds, Color32 color, ref Vector3 minBounds, ref Vector3 maxBounds)
         {
             var firstVertexIndex = verts.Count;
 
             verts.EnsureCapacity(verts.Count + area.Corners.Length);
-            inds.EnsureCapacity(inds.Count + area.Corners.Length + 1);
 
             foreach (var position in area.Corners)
             {
@@ -71,11 +94,7 @@ namespace ValveResourceFormat.Renderer.SceneNodes
                 verts.Add(new(position, color, Vector3.UnitZ));
             }
 
-            for (var i = 0; i < area.Corners.Length; i++)
-            {
-                inds.Add(firstVertexIndex + i);
-            }
-            inds.Add(int.MaxValue);
+            AddPolygon(firstVertexIndex, area.Corners.Length, triangleInds, lineInds);
         }
 
         /// <inheritdoc/>
@@ -103,13 +122,10 @@ namespace ValveResourceFormat.Renderer.SceneNodes
             GL.Enable(EnableCap.PolygonOffsetFill);
             GL.PolygonOffsetClamp(0, 96, 0.0005f);
 
-            GL.Enable(EnableCap.PrimitiveRestart);
-            GL.PrimitiveRestartIndex(int.MaxValue);
-            GL.DrawElements(PrimitiveType.LineLoop, indexCount, DrawElementsType.UnsignedInt, 0);
+            GL.DrawElements(PrimitiveType.Lines, indexCount - triangleIndexCount, DrawElementsType.UnsignedInt, triangleIndexCount * sizeof(int));
 
-            GL.DrawElementsInstancedBaseInstance(PrimitiveType.TriangleFan, indexCount, DrawElementsType.UnsignedInt, 0, 1, Id);
+            GL.DrawElementsInstancedBaseInstance(PrimitiveType.Triangles, triangleIndexCount, DrawElementsType.UnsignedInt, 0, 1, Id);
 
-            GL.Disable(EnableCap.PrimitiveRestart);
             GL.Disable(EnableCap.PolygonOffsetLine);
             GL.Disable(EnableCap.PolygonOffsetFill);
             GL.PolygonOffsetClamp(0, 0, 0);
@@ -129,7 +145,8 @@ namespace ValveResourceFormat.Renderer.SceneNodes
             }
 
             var verts = new List<SimpleVertexNormal>();
-            var inds = new List<int>();
+            var triangleInds = new List<int>();
+            var lineInds = new List<int>();
 
             if (navMeshFile.GenerationParams != null)
             {
@@ -145,10 +162,10 @@ namespace ValveResourceFormat.Renderer.SceneNodes
                     var maxBounds = new Vector3(float.MaxValue);
                     foreach (var area in hullAreas)
                     {
-                        AddArea(area, verts, inds, NavMeshColor, ref minBounds, ref maxBounds);
+                        AddArea(area, verts, triangleInds, lineInds, NavMeshColor, ref minBounds, ref maxBounds);
                     }
 
-                    var sceneNode = new NavMeshSceneNode(scene, verts, inds)
+                    var sceneNode = new NavMeshSceneNode(scene, verts, triangleInds, lineInds)
                     {
                         LayerName = $"Navigation mesh (hull {i})",
                         LocalBoundingBox = new AABB(minBounds, maxBounds),
@@ -156,7 +173,8 @@ namespace ValveResourceFormat.Renderer.SceneNodes
                     scene.Add(sceneNode, false);
 
                     verts.Clear();
-                    inds.Clear();
+                    triangleInds.Clear();
+                    lineInds.Clear();
                 }
             }
 
@@ -167,10 +185,10 @@ namespace ValveResourceFormat.Renderer.SceneNodes
 
                 foreach (var ladder in navMeshFile.Ladders)
                 {
-                    AddLadder(ladder, verts, inds, NavMeshLadderColor, ref minBounds, ref maxBounds);
+                    AddLadder(ladder, verts, triangleInds, lineInds, NavMeshLadderColor, ref minBounds, ref maxBounds);
                 }
 
-                var laddersSceneNode = new NavMeshSceneNode(scene, verts, inds)
+                var laddersSceneNode = new NavMeshSceneNode(scene, verts, triangleInds, lineInds)
                 {
                     LayerName = "Navigation mesh (ladders)",
                     LocalBoundingBox = new AABB(minBounds, maxBounds),
