611bfc74638a4025caabd3df585639d00a612fdf
w1tcherrr <70418164+w1tcherrr@users.noreply.github.com>
2026-09-04 00:41:06 +0200

    Read single-object generic data lists during model export

--- ValveResourceFormat/IO/Extract/ModelExtract.GameData.cs
+++ ValveResourceFormat/IO/Extract/ModelExtract.GameData.cs
@@ -189,13 +189,23 @@ partial class ModelExtract
         foreach (var genericDataClass in genericDataClassesList)
         {
             var dataKey = genericDataClass.ListKey;
-            if (keyvalues.ContainsKey(dataKey))
+            if (!keyvalues.ContainsKey(dataKey))
             {
-                var genericDataList = keyvalues.GetArray(dataKey);
-                foreach (var genericData in genericDataList!)
+                continue;
+            }
+
+            var genericDataList = keyvalues.GetArray(dataKey);
+            if (genericDataList != null)
+            {
+                foreach (var genericData in genericDataList)
                 {
                     AddGenericGameData(lists.GameData, genericDataClass.Class, genericData);
                 }
+            }
+            else if (keyvalues.GetSubCollection(dataKey) is { } singleGenericData)
+            {
+                // Some models author this as a single KV object instead of a one-element list.
+                AddGenericGameData(lists.GameData, genericDataClass.Class, singleGenericData);
             }
         }
 
