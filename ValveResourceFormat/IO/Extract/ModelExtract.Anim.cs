8518307514be31d8cb8ec7aaee9e447af63e7f6e
w1tcherrr <70418164+w1tcherrr@users.noreply.github.com>
2026-09-04 00:41:11 +0200

    Leave the cloth proxy bones the compiler rebuilds out of the emitted skeleton

--- ValveResourceFormat/IO/Extract/ModelExtract.Anim.cs
+++ ValveResourceFormat/IO/Extract/ModelExtract.Anim.cs
@@ -385,6 +385,11 @@ partial class ModelExtract
 
         foreach (var bone in skeleton.Bones)
         {
+            if (IsGeneratedClothProxyBone(bone))
+            {
+                continue;
+            }
+
             var transform = transforms[bone.Index];
             var boneName = GetExportBoneName(bone);
 
