afe6e0557285ed7ffffb8a8fedb7bba3fe721a6e
w1tcherrr <70418164+w1tcherrr@users.noreply.github.com>
2026-09-04 00:41:08 +0200

    Read the joints of a compiled physics aggregate

--- ValveResourceFormat/Resource/ResourceTypes/RubikonPhysics/Shapes/Mesh.cs
+++ ValveResourceFormat/Resource/ResourceTypes/RubikonPhysics/Shapes/Mesh.cs
@@ -44,7 +44,7 @@ namespace ValveResourceFormat.ResourceTypes.RubikonPhysics.Shapes
             public NodeType Type => (NodeType)(PackedTypeChildOffset >> 30);
 
             /// <summary>
-            /// The 2nd child offset, otherwise when <c>Type</c> is <see cref="NodeType.Leaf" />, this is the triangle count.
+            /// The 2nd child offset, otherwise when <see cref="Type" /> is <see cref="NodeType.Leaf" />, this is the triangle count.
             /// </summary>
             public uint ChildOffset => PackedTypeChildOffset & 0x3FFFFFFF;
 
