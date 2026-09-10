85a7cc675a59f1707ffb28e13d728c5afdaa3776
w1tcherrr <70418164+w1tcherrr@users.noreply.github.com>
2026-09-10 22:19:35 +0200

    Use the ValvePak namespace in the resourcecompiler round-trip test

--- Tests/ResourceCompilerTest.cs
+++ Tests/ResourceCompilerTest.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SteamDatabase.ValvePak;
+using ValvePak;
 using ValveResourceFormat;
 using ValveResourceFormat.IO;
 using ValveResourceFormat.ResourceTypes;
